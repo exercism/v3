0829e7b470fd1bba66910059d2bc18085efde8b3
Erik Schierboom <erik_schierboom@hotmail.com>
2020-01-09 13:12:43 +0100

    csharp: bitwise operations to fact-base

--- languages/csharp/exercises/concept/bitwise-operations/BitwiseOperationsTest.cs
+++ languages/csharp/exercises/concept/bitwise-operations/BitwiseOperationsTest.cs
@@ -1,67 +1,92 @@
 using Xunit;
 
-// TODO: convert Theory-based tests to Fact-based tests.
-// This is necessary in order to be able to display the 
-// input for which the test failed, which is defined in
-// the .meta/config.json file
 public class PermissionsTest
 {
-    [Theory]
-    [InlineData(AccountType.Guest, Permission.Read)]
-    [InlineData(AccountType.User, Permission.Read | Permission.Write)]
-    [InlineData(AccountType.Moderator, Permission.Read | Permission.Write | Permission.Delete)]
-    public void Default(AccountType accountType, Permission expected) =>
-        Assert.Equal(expected, Permissions.Default(accountType));
+    [Fact]
+    public void DefaultForGuest() =>
+        Assert.Equal(Permission.Read, Permissions.Default(AccountType.Guest));
 
-    [Theory]
-    [InlineData((AccountType)123)]
-    [InlineData((AccountType)7)]
-    public void DefaultForUnknownAccountType(AccountType accountType) =>
-        Assert.Equal(Permission.None, Permissions.Default(accountType));
+    [Fact]
+    public void DefaultForUser() =>
+        Assert.Equal(Permission.Read | Permission.Write, Permissions.Default(AccountType.User));
 
-    [Theory]
-    [InlineData(Permission.None, Permission.Read, Permission.Read)]
-    [InlineData(Permission.Read, Permission.Read, Permission.Read)]
-    [InlineData(Permission.Read, Permission.None, Permission.Read)]
-    [InlineData(Permission.Write, Permission.Read, Permission.Write | Permission.Read)]
-    [InlineData(Permission.Delete, Permission.Read, Permission.Delete | Permission.Read)]
-    [InlineData(Permission.Read | Permission.Write, Permission.Delete, Permission.Read | Permission.Write | Permission.Delete)]
-    [InlineData(Permission.All, Permission.None, Permission.All)]
-    [InlineData(Permission.All, Permission.Read, Permission.All)]
-    public void Grant(Permission permission, Permission permissionToGrant, Permission expected) =>
-        Assert.Equal(expected, Permissions.Grant(permission, permissionToGrant));
+    [Fact]
+    public void DefaultForModerator() =>
+        Assert.Equal(Permission.Read | Permission.Write | Permission.Delete, Permissions.Default(AccountType.Moderator));
 
-    [Theory]
-    [InlineData(Permission.None, Permission.Read, Permission.None)]
-    [InlineData(Permission.Read, Permission.Write, Permission.Read)]
-    [InlineData(Permission.Read, Permission.All, Permission.None)]
-    [InlineData(Permission.Write, Permission.None, Permission.Write)]
-    [InlineData(Permission.Delete, Permission.Delete, Permission.None)]
-    [InlineData(Permission.Read | Permission.Write, Permission.Write, Permission.Read)]
-    [InlineData(Permission.All, Permission.Delete, Permission.Read | Permission.Write)]
-    [InlineData(Permission.All, Permission.Read | Permission.Write, Permission.Delete)]
-    public void Revoke(Permission permission, Permission permissionToRevoke, Permission expected) =>
-        Assert.Equal(expected, Permissions.Revoke(permission, permissionToRevoke));
+    [Fact]
+    public void DefaultForUnknown() =>
+        Assert.Equal(Permission.None, Permissions.Default((AccountType)123));
 
-    [Theory]
-    [InlineData(Permission.None, Permission.Read, false)]
-    [InlineData(Permission.None, Permission.Write, false)]
-    [InlineData(Permission.None, Permission.Delete, false)]
-    [InlineData(Permission.None, Permission.All, false)]
-    [InlineData(Permission.All, Permission.Read, true)]
-    [InlineData(Permission.All, Permission.Write, true)]
-    [InlineData(Permission.All, Permission.Delete, true)]
-    [InlineData(Permission.All, Permission.All, true)]
-    [InlineData(Permission.Read, Permission.Read, true)]
-    [InlineData(Permission.Write, Permission.Write, true)]
-    [InlineData(Permission.Delete, Permission.Delete, true)]
-    [InlineData(Permission.Read | Permission.Write, Permission.Read, true)]
-    [InlineData(Permission.Read | Permission.Write, Permission.Write, true)]
-    [InlineData(Permission.Read | Permission.Write, Permission.Delete, false)]
-    [InlineData(Permission.Read | Permission.Write, Permission.All, false)]
-    [InlineData(Permission.Write | Permission.Delete, Permission.Read, false)]
-    [InlineData(Permission.Write | Permission.Delete, Permission.Write, true)]
-    [InlineData(Permission.Delete | Permission.Read, Permission.Delete, true)]
-    public void Check(Permission owned, Permission desired, bool expected) =>
-        Assert.Equal(expected, Permissions.Check(owned, desired));
+    [Fact]
+    public void GrantReadToNone() =>
+        Assert.Equal(Permission.Read, Permissions.Grant(Permission.None, Permission.Read));
+
+    [Fact]
+    public void GrantReadToRead() =>
+        Assert.Equal(Permission.Read, Permissions.Grant(Permission.Read, Permission.Read));
+
+    [Fact]
+    public void GrantAllToNone() =>
+        Assert.Equal(Permission.All, Permissions.Grant(Permission.None, Permission.All));
+
+    [Fact]
+    public void GrantDeleteToReadAndWrite() =>
+        Assert.Equal(Permission.All, Permissions.Grant(Permission.Read | Permission.Write, Permission.Delete));
+
+    [Fact]
+    public void GrantReadAndWriteToNone() =>
+        Assert.Equal(Permission.Read | Permission.Write, Permissions.Grant(Permission.None, Permission.Read | Permission.Write));
+
+    [Fact]
+    public void RevokeNoneFromRead() =>
+        Assert.Equal(Permission.Read, Permissions.Revoke(Permission.Read, Permission.None));
+
+    [Fact]
+    public void RevokeWriteFromWrite() =>
+        Assert.Equal(Permission.None, Permissions.Revoke(Permission.Write, Permission.Write));
+
+    [Fact]
+    public void RevokeDeleteFromAll() =>
+        Assert.Equal(Permission.Read | Permission.Write, Permissions.Revoke(Permission.All, Permission.Delete));
+
+    [Fact]
+    public void RevokeReadAndWriteFromWriteAndDelete() =>
+        Assert.Equal(Permission.Delete, Permissions.Revoke(Permission.Write | Permission.Delete, Permission.Read | Permission.Write));
+
+    [Fact]
+    public void RevokeAllFromReadAndWrite() =>
+        Assert.Equal(Permission.None, Permissions.Revoke(Permission.Read | Permission.Write, Permission.All));
+
+    [Fact]
+    public void CheckNoneForRead() =>
+        Assert.False(Permissions.Check(Permission.None, Permission.Read));
+
+    [Fact]
+    public void CheckWriteForWrite() =>
+        Assert.True(Permissions.Check(Permission.Write, Permission.Write));
+
+    [Fact]
+    public void CheckAllForWrite() =>
+        Assert.True(Permissions.Check(Permission.All, Permission.Write));
+
+    [Fact]
+    public void CheckReadAndWriteForRead() =>
+        Assert.True(Permissions.Check(Permission.Read | Permission.Write, Permission.Read));
+
+    [Fact]
+    public void CheckAllForReadAndWrite() =>
+        Assert.True(Permissions.Check(Permission.Read | Permission.Write, Permission.Read | Permission.Write));
+
+    [Fact]
+    public void CheckReadAndWriteForReadAndWrite() =>
+        Assert.True(Permissions.Check(Permission.Read | Permission.Write, Permission.Read | Permission.Write));
+
+    [Fact]
+    public void CheckReadAndWriteForReadAndDelete() =>
+        Assert.False(Permissions.Check(Permission.Read | Permission.Write, Permission.Read | Permission.Delete));
+
+    [Fact]
+    public void CheckReadAndWriteAndDeleteForAll() =>
+        Assert.True(Permissions.Check(Permission.Read | Permission.Write | Permission.Delete, Permission.All));
 }
