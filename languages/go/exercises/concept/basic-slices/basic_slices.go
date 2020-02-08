8e403434bc05d7eb202eea6a9c7e4efe1c37669e
Gabriel Nelle <tehsphinx@web.de>
2020-02-08 12:14:02 +0100

    Rephrase `not implemented` panics

--- languages/go/exercises/concept/basic-slices/basic_slices.go
+++ languages/go/exercises/concept/basic-slices/basic_slices.go
@@ -3,34 +3,34 @@ package basic_slices
 // GetItem retrieves an item from a slice at given position. The second return value indicates whether
 // a the given index existed in the slice or not.
 func GetItem(slice []uint8, index int) (uint8, bool) {
-	panic("GetItem not implemented")
+	panic("Please implement the GetItem function")
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range it is be appended.
 func SetItem(slice []uint8, index int, value uint8) []uint8 {
-	panic("SetItem not implemented")
+	panic("Please implement the SetItem function")
 }
 
 // PrefilledSlice creates a slice of given length and prefills it with the given value.
 func PrefilledSlice(value, length int) []int {
-	panic("PrefilledSlice not implemented")
+	panic("Please implement the PrefilledSlice function")
 }
 
 // NumberRow fills a slice with the numbers 1, 2, 3, etc. It returns as many numbers as are necessary for the sum
 // of the slice to be equal or larger than `sumMin`.
 func NumberRow(sumMin int) []int {
-	panic("NumberRow not implemented")
+	panic("Please implement the NumberRow function")
 }
 
 // RemoveItemPure removes an item without changing the input values or other side effects.
 // In functional programming this is called a `pure` function.
 func RemoveItemPure(slice []int, index int) []int {
-	panic("RemoveItemPure not implemented")
+	panic("Please implement the RemoveItemPure function")
 }
 
 // RemoveItem removes an item from a slice by modifying the existing slice (without allocating new memory).
 // The order of items in the slice might not be the same as before.
 func RemoveItem(slice []int, index int) []int {
-	panic("RemoveItem not implemented")
+	panic("Please implement the RemoveItem function")
 }
