2e29b7db1c1c11f9e929532ea8cf0e8d9a1def5c
A.L. Drake <a.l.drake713@gmail.com>
2020-02-20 21:40:39 -0700

    Add third function, reformat.

--- languages/python/exercises/concept/strings/example.py
+++ languages/python/exercises/concept/strings/example.py
@@ -1,6 +1,23 @@
+import re
+
+LOGLINE_RE = r"\[(\w*)\] (.*)"
+
+
+def _extract_pieces(message):
+    pieces = re.search(LOGLINE_RE, message)
+    return pieces.group(1), pieces.group(2)
+
+
 def change_log_level(message, new_loglevel):
     """Change loglevel of message to new_loglevel."""
     return f"[{new_loglevel}] {extract_message(message)}"
 
+
 def extract_message(message):
-    return message.split("]")[1].strip()
+    _, msg = _extract_pieces(message)
+    return msg
+
+
+def reformat(message):
+    loglevel, msg = _extract_pieces(message)
+    return f"{msg} ({loglevel.lower()})"
