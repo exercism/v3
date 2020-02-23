ab15aa5b628177d1fa69c32315dd5e1dfdca63ca
Carl Steib <carl.steib@gmail.com>
2020-02-22 20:57:16 -0800

    Concept: lists (v2) (#785)
    * Rename exercise from "list" to "lists"
    
    * Add config.json
    
    * Update design.md leaning objectives
    
    * Add intro of quoting and evaluation to introduction.md
    
    * Add summary of leaning objectives to after.md
    
    * Remove sorting and querying from instructions
    
    * Adjust exercise to scope of design.md
    
    * Update hints.md
    
    * Fix typos etc.
    
    * Fix dead links
    
    * Fix other dead link
    
    * Update languages/clojure/exercises/concept/lists/.doc/hints.md
    
    Co-Authored-By: Bobby Towers <porkostomus@mail.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Bobby Towers <porkostomus@mail.com>
    
    * Apply more suggestions from code review
    
    Co-authored-by: Bobby Towers <porkostomus@mail.com>

---
+++ languages/clojure/exercises/concept/lists/.meta/example.clj
@@ -0,0 +1,29 @@
+(ns lists)
+
+(defn new-list []
+  '())
+
+(defn add-language
+  [lang lang-list]
+  (cons lang lang-list))
+
+(defn first-lanugage
+  [lang-list]
+  (first lang-list))
+
+(defn remove-language
+  [lang-list]
+  (rest lang-list))
+
+(defn count-languages
+  [lang-list]
+  (count lang-list))
+
+(defn learning-list []
+  (->> (empty-list)
+       (add-language "Clojure")
+       (add-language "Lisp")
+       remove-language
+       (add-language "Java")
+       (add-language "Javascript")
+       count-languages))
