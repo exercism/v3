dc4ab3377081d193e04c8df50e75b5c7238f68ae
Denys M <dector9@gmail.com>
2020-02-29 18:05:10 +0400

    [Kotlin] Create composite root build for exercises

---
+++ languages/kotlin/settings.gradle.kts
@@ -0,0 +1,11 @@
+// Collect concept exercises
+File("exercises/concept").listFiles()?.forEach { dir ->
+    val isGradleProject = dir
+        .resolve("build.gradle.kts")
+        .exists()
+
+    if (isGradleProject) {
+        val relativeFile = dir.relativeTo(File("."))
+        includeBuild(relativeFile.path)
+    }
+}
