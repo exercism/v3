d754afb9cfa452b70d5e801a0367b0166c623122
Brooks J Rady <b.j.rady@gmail.com>
2020-03-10 11:15:41 +0000

    Remove the nils from the end of the stub code

--- languages/common-lisp/exercises/concept/sexpr/sexpr.lisp
+++ languages/common-lisp/exercises/concept/sexpr/sexpr.lisp
@@ -5,17 +5,13 @@
 (in-package :sexpr)
 
 (defun is-an-atom-p (thing)
-  "Evaluates to T if THING is an atom, NIL otherwise"
-  nil)
+  "Evaluates to T if THING is an atom, NIL otherwise")
 
 (defun is-a-cons-p (thing)
-  "Evaluates to T if THING is a cons, NIL otherwise"
-  nil)
+  "Evaluates to T if THING is a cons, NIL otherwise")
 
 (defun first-thing (cons)
-  "Returns the first part of CONS"
-  nil)
+  "Returns the first part of CONS")
 
 (defun rest-of-it (cons)
-  "Returns the 'rest' of the CONS"
-  nil)
+  "Returns the 'rest' of the CONS")
