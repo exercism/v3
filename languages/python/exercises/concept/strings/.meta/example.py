286fc893c4c754df0ac0da5f7d97a59349d0ab63
A.L. Drake <a.l.drake713@gmail.com>
2020-03-11 20:32:58 -0600

    Single spacing between functions.

--- languages/python/exercises/concept/strings/.meta/example.py
+++ languages/python/exercises/concept/strings/.meta/example.py
@@ -15,11 +15,9 @@ def _extract_pieces_no_regex(message):
     words = [word for word in message.strip().replace("]", "[").split("[") if word]
     return words[0], words[1].strip()
 
-
 def change_log_level(message, new_loglevel):
     """Change loglevel of message to new_loglevel."""
     return f"[{new_loglevel}] {extract_message(message)}"
-
 
 def extract_message(message):
     return _extract_pieces_no_regex(message)[1]
