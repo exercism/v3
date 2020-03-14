7c9e87310898a0a72440d67624f721dd816a4830
Eli Flanagan <efx@users.noreply.github.com>
2020-03-14 16:47:28 -0400

    Update languages/rust/exercises/concept/enums/src/lib.rs
    Co-Authored-By: Peter Goodspeed-Niklaus <coriolinus@users.noreply.github.com>

--- languages/rust/exercises/concept/enums/src/lib.rs
+++ languages/rust/exercises/concept/enums/src/lib.rs
@@ -1,7 +1,7 @@
 // TODO define enum variants
 pub enum LogLevel {}
 
-pub fn parse_log_level(log: &str) -> LogLevel {
+pub fn parse_log_level(log: &str) -> Option<LogLevel> {
     unimplemented!("parse a log and return the level")
 }
 pub fn output_for_short_log(kind: LogLevel, log: &str) -> String {
