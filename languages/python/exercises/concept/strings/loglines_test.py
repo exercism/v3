b2f2ab4d08b88fb46a16fa7e9948066af7d89c8d
aldraco <a.l.drake713@gmail.com>
2020-03-20 15:05:13 -0600

    Use msg parameter for assert statements in test.

--- languages/python/exercises/concept/strings/loglines_test.py
+++ languages/python/exercises/concept/strings/loglines_test.py
@@ -7,47 +7,47 @@ class TestLogLines(unittest.TestCase):
         self.assertEqual(
             extract_message("[INFO] Hello there."),
             "Hello there.",
-            "Should correctly extract a basic message.",
+            msg="Should correctly extract a basic message.",
         )
 
     def test_message_with_punctuation(self):
         self.assertEqual(
             extract_message("[WARN] File not found: exercism_practice.py"),
             "File not found: exercism_practice.py",
-            "Should preserve punctuation and whitespace from original message.",
+            msg="Should preserve punctuation and whitespace from original message.",
         )
 
     def test_level_word_remains_in_message(self):
         self.assertEqual(
             extract_message("[ERROR] Error while serializing data."),
             "Error while serializing data.",
-            "Should preserve a loglevel word that is actually part of the message.",
+            msg="Should preserve a loglevel word that is actually part of the message.",
         )
 
     def test_change_loglevel(self):
         self.assertEqual(
             change_log_level("[ERROR] No configs found, but not a big deal.", "INFO"),
             "[INFO] No configs found, but not a big deal.",
-            "Should replace the loglevel.",
+            msg="Should replace the loglevel.",
         )
 
     def test_change_loglevel_with_loglevel_in_message(self):
         self.assertEqual(
             change_log_level("[WARN] Warning: file does not exist.", "INFO"),
             "[INFO] Warning: file does not exist.",
-            "Should not replace loglevel names that are part of the message.",
+            msg="Should not replace loglevel names that are part of the message.",
         )
 
     def test_reformat(self):
         self.assertEqual(
             reformat("[WARN] Warning: file not found."),
             "Warning: file not found. (warn)",
-            "Should reformat with lowercase loglevel. ",
+            msg="Should reformat with lowercase loglevel. ",
         )
 
     def test_capture_correct_loglevel(self):
         self.assertEqual(
             extract_message("[WARN] Warning: file not found. Will WARN once."),
             "Warning: file not found. Will WARN once.",
-            "Should extract loglevel inside brackets, not from messsage.",
+            msg="Should extract loglevel inside brackets, not from messsage.",
         )
