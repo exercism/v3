2fe93840027bce6189eee0cfdce8cc0e56615ef4
Erik Schierboom <erik_schierboom@hotmail.com>
2020-03-25 09:54:11 +0100

    Skip

--- languages/fsharp/exercises/concept/basics/BasicsTests.fs
+++ languages/fsharp/exercises/concept/basics/BasicsTests.fs
@@ -8,17 +8,17 @@ open Basics
 [<Fact>]
 let ``Expected minutes in oven``() = expectedMinutesInOven |> should equal 40
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Remaining minutes in oven``() = remainingMinutesInOven 25 |> should equal 15
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Preparation time in minutes for one layer``() = preparationTimeInMinutes 1 |> should equal 2
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Preparation time in minutes for multiple layers``() = preparationTimeInMinutes 4 |> should equal 8
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Total time in minutes for one layer``() = totalTimeInMinutes 1 30 |> should equal 32
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Total time in minutes for multiple layers``() = totalTimeInMinutes 4 8 |> should equal 16
