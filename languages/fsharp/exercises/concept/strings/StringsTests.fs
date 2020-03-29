44460c5bfcc7341d626e85d9a97bab5f1e9d7cd2
Erik Schierboom <erik_schierboom@hotmail.com>
2020-03-29 19:18:28 +0200

    [F#] Add skip to tests (#995)

--- languages/fsharp/exercises/concept/strings/StringsTests.fs
+++ languages/fsharp/exercises/concept/strings/StringsTests.fs
@@ -8,37 +8,37 @@ open Strings
 [<Fact>]
 let ``Error message``() = message "[ERROR]: Stack overflow" |> should equal "Stack overflow"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Warning message``() = message "[WARNING]: Disk almost full" |> should equal "Disk almost full"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Info message``() = message "[INFO]: File moved" |> should equal "File moved"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Message with leading and trailing white space``() =
     message "[WARNING]:   \tTimezone not set  \r\n"
     |> should equal "Timezone not set"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Error log level``() = logLevel "[ERROR]: Disk full" |> should equal "error"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Warning log level``() = logLevel "[WARNING]: Unsafe password" |> should equal "warning"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Info log level``() = logLevel "[INFO]: Timezone changed" |> should equal "info"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Error reformat``() = reformat "[ERROR]: Segmentation fault" |> should equal "Segmentation fault (error)"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Warning reformat``() =
     reformat "[WARNING]: Decreased performance" |> should equal "Decreased performance (warning)"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Info reformat``() = reformat "[INFO]: Disk defragmented" |> should equal "Disk defragmented (info)"
 
-[<Fact>]
+[<Fact(Skip = "Remove to run test")>]
 let ``Reformat with leading and trailing white space``() =
     reformat "[ERROR]: \t Corrupt disk\t \t \r\n"
     |> should equal "Corrupt disk (error)"
