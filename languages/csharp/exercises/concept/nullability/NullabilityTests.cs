6e0d4849a6df08772145663767aafa7c356ab4f2
Marcos Almeida <marcos.almeida@xcomponent.com>
2020-03-30 18:55:40 +0200

    issue961: Nullability.cs: apply rename on other files

--- languages/csharp/exercises/concept/nullability/NullabilityTests.cs
+++ languages/csharp/exercises/concept/nullability/NullabilityTests.cs
@@ -4,30 +4,30 @@ public class NullabilityTests
 {
     [Fact]
     public void ComputeWidthPx() =>
-        Assert.Equal(7*12+5*12+3*12+2*12, Nullability.ComputeWidthPx("Matilda", "Leana", "Cox", 12));
+        Assert.Equal(7*12+5*12+3*12+2*12, Badge.ComputeWidthPx("Matilda", "Leana", "Cox", 12));
 
     [Fact]
     public void MissingMiddleName() =>
-        Assert.Equal(7*12+3*12+12, Nullability.ComputeWidthPx("Matilda", null, "Cox", 12));
+        Assert.Equal(7*12+3*12+12, Badge.ComputeWidthPx("Matilda", null, "Cox", 12));
 
     [Fact]
     public void OnlyFirstName() =>
-        Assert.Equal(7*12, Nullability.ComputeWidthPx("Matilda", null, null, 12));
+        Assert.Equal(7*12, Badge.ComputeWidthPx("Matilda", null, null, 12));
 
     [Fact]
     public void OnlyLastName() =>
-        Assert.Equal(3*12, Nullability.ComputeWidthPx(null, null, "Cox", 12));
+        Assert.Equal(3*12, Badge.ComputeWidthPx(null, null, "Cox", 12));
 
     [Fact]
     public void DifferentFontSize() =>
-        Assert.Equal(3*17, Nullability.ComputeWidthPx(null, null, "Cox", 17));
+        Assert.Equal(3*17, Badge.ComputeWidthPx(null, null, "Cox", 17));
 
     [Fact]
     public void NothingToDisplay() =>
-        Assert.Equal(0, Nullability.ComputeWidthPx(null, null, null, 17));
+        Assert.Equal(0, Badge.ComputeWidthPx(null, null, null, 17));
 
     [Fact]
     public void NoFontSize() =>
-        Assert.Equal(0, Nullability.ComputeWidthPx("Matilda", null, null, null));
+        Assert.Equal(0, Badge.ComputeWidthPx("Matilda", null, null, null));
 
 }
