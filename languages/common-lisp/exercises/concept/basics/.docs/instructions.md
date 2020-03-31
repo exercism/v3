b1a15a0e8e9ec0f10e78ef5b99ee0a372a511266
Brooks J Rady <b.j.rady@gmail.com>
2020-04-01 00:09:26 +0100

    First draft of the basics exercise

--- languages/common-lisp/exercises/concept/basics/.docs/instructions.md
+++ languages/common-lisp/exercises/concept/basics/.docs/instructions.md
@@ -1,1 +1,17 @@
-In this exercise you will write predicate functions to determine if things are atoms or conses. To do this you will simply use built-in Common Lisp functions. Also you will learn how to access the parts of a cons.
+In this exercise, you fill need to fill in a number of incomplete functions.
+
+## Symbols and Quoting
+
+In this section, you should write two functions: one that returns a symbol, and
+another that returns a keyword.
+
+## Truthy and Falsy Values
+
+For this section, you should write two functions: one that returns true, and
+another that returns false.
+
+## S-Expressions
+
+In this section of the exercise, you should write predicate functions to
+determine if things are atoms or conses. To do this you can simply use built-in
+Common Lisp functions. You will also learn how to access the parts of a cons.
--- languages/common-lisp/exercises/concept/basics/.meta/example.lisp
+++ languages/common-lisp/exercises/concept/basics/.meta/example.lisp
@@ -1,20 +1,38 @@
-(defpackage #:sexpr
+(defpackage #:basics
   (:use :cl)
-  (:export :is-an-atom-p :is-a-cons-p :first-thing :rest-of-it))
-(in-package :sexpr)
+  (:export :gimme-bar-symbol :gimme-baz-keyword :speak-the-truth :lie-to-me
+           :is-an-atom-p :is-a-cons-p :first-thing :rest-of-it))
 
+(in-package :basics)
+
+;; Returns the symbol BAR
+(defun gimme-bar-symbol ()
+  'bar)
+
+;; Returns the keyword BAZ
+(defun gimme-baz-keyword ()
+  :baz)
+
+;; Return something true
+(defun speak-the-truth ()
+  'this-statement-is-false)
+
+;; Return something false
+(defun lie-to-me ()
+  '())
+
+;; Evaluates to T if THING is an atom, NIL otherwise
 (defun is-an-atom-p (thing)
-  "Evaluates to T if THING is an atom, NIL otherwise"
   (atom thing))
 
+;; Evaluates to T if THING is a cons, NIL otherwise
 (defun is-a-cons-p (thing)
-  "Evaluates to T if THING is a cons, NIL otherwise"
   (consp thing))
 
+;; Returns the first part of CONS
 (defun first-thing (cons)
-  "Returns the first part of CONS"
   (car cons))
 
+;; Returns the 'rest' of the CONS
 (defun rest-of-it (cons)
-  "Returns the 'rest' of the CONS"
   (cdr cons))
