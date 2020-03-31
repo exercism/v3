9931ef6149f1b2782033c6cabeb1439aeb100289
Mike May <mikemay.mdamay@googlemail.com>
2020-03-31 11:44:15 +0100

    Apply suggestions from code review
    Co-Authored-By: Erik Schierboom <erik_schierboom@hotmail.com>

--- languages/csharp/exercises/concept/properties/PropertiesTests.cs
+++ languages/csharp/exercises/concept/properties/PropertiesTests.cs
@@ -8,7 +8,7 @@ public class WeighingMachineTests
     {
         var wm = new WeighingMachine();
         wm.InputWeight = 60m;
-        Assert.Equal(60m, wm.InputWeight, 3 );
+        Assert.Equal(60m, wm.InputWeight, precision: 3);
     }
 
     [Fact(Skip = "Remove to run test")]
@@ -18,43 +18,47 @@ public class WeighingMachineTests
         Assert.Throws<ArgumentException>(() => wm.InputWeight = -10);
     }
     
-    [Fact( Skip = "Remove to run test")]
+    [Fact(Skip = "Remove to run test")]
     public void Get_US_display_weight()
     {
         var wm = new WeighingMachine();
         wm.InputWeight = 60m;
-        Assert.Equal((132, 4), (wm.USDisplayWeight.Pounds, wm.USDisplayWeight.Ounces) );
+        Assert.Equal((132, 4), (wm.USDisplayWeight.Pounds, wm.USDisplayWeight.Ounces));
     }
-    [Fact( Skip = "Remove to run test")]
+
+    [Fact(Skip = "Remove to run test")]
     public void Input_pounds_and_get_US_display_weight()
     {
         var wm = new WeighingMachine();
         wm.Units = Units.Pounds;
         wm.InputWeight = 175.5m;
-        Assert.Equal((175, 8), (wm.USDisplayWeight.Pounds, wm.USDisplayWeight.Ounces) );
+        Assert.Equal((175, 8), (wm.USDisplayWeight.Pounds, wm.USDisplayWeight.Ounces));
     }
-    [Fact( Skip = "Remove to run test")]
+
+    [Fact(Skip = "Remove to run test")]
     public void Apply_tare_adjustment_and_get_display_weight()
     {
         var wm = new WeighingMachine();
         wm.InputWeight = 100;
         wm.TareAdjustment = 10;
-        Assert.Equal(90, wm.DisplayWeight );
+        Assert.Equal(90, wm.DisplayWeight);
     }
-    [Fact( Skip = "Remove to run test")]
+
+    [Fact(Skip = "Remove to run test")]
     public void Apply_negative_tare_adjustment()
     {
         var wm = new WeighingMachine();
         wm.InputWeight = 100;
         wm.TareAdjustment = -10;
-        Assert.Equal(110, wm.DisplayWeight );
+        Assert.Equal(110, wm.DisplayWeight);
     }
-    [Fact( Skip = "Remove to run test")]
+
+    [Fact(Skip = "Remove to run test")]
     public void Apply_large_tare_adjustment_to_allow_negative_display_weight()
     {
         var wm = new WeighingMachine();
         wm.InputWeight = 100;
         wm.TareAdjustment = 110;
-        Assert.Equal(-10, wm.DisplayWeight );
+        Assert.Equal(-10, wm.DisplayWeight);
     }
 }
