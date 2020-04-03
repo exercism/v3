9f6cde7889df464b42d341bfa30d6de62506075d
wolf99 <wolf99@users.noreply.github.com>
2020-04-03 07:32:37 +0100

    Improve Skip string #1047 (#1083)
    Co-authored-by: wolf99 <wol99@users.noreply.github.com>

--- languages/fsharp/exercises/concept/lists/ListsTests.fs
+++ languages/fsharp/exercises/concept/lists/ListsTests.fs
@@ -10,42 +10,42 @@ let ``New list``() =
     let expected: string list = []
     newList |> should equal expected
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Existing list``() = existingList |> should equal [ "F#"; "Clojure"; "Haskell" ]
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Add language to new list``() = addLanguage "Scala" newList |> should equal [ "Scala" ]
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Add language to existing list``() =
     addLanguage "Common Lisp" existingList |> should equal [ "Common Lisp"; "F#"; "Clojure"; "Haskell" ]
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Add language to custom list``() = addLanguage "Racket" [ "Scheme" ] |> should equal [ "Racket"; "Scheme" ]
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Last added language of existing list``() = lastAddedLanguage existingList |> should equal "F#"
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Last added language of custom list``() = lastAddedLanguage [ "Crystal"; "Nim" ] |> should equal "Crystal"
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Count languages on new list``() = countLanguages newList |> should equal 0
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Count languages on existing list``() = countLanguages existingList |> should equal 3
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Count languages on custom list``() = countLanguages [ "Python"; "JavaScript" ] |> should equal 2
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Reverse order of new list``() =
     let expected: string list = []
     reverseList newList |> should equal expected
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Reverse order of existing list``() = reverseList existingList |> should equal [ "Haskell"; "Clojure"; "F#" ]
 
-[<Fact(Skip = "Remove to run test")>]
+[<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Reverse order of custom list``() =
     reverseList [ "Kotlin"; "Java"; "Scala"; "Clojure" ] |> should equal [ "Clojure"; "Scala"; "Java"; "Kotlin" ]
