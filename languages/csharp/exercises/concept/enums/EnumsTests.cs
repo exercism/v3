6f0d476edfe1f8acc96a114dd175e98a79c05afa
Hani <hani.herbert.ho@gmail.com>
2020-04-06 16:24:48 +0800

    Fixed EnumsTests.cs

--- languages/csharp/exercises/concept/enums/EnumsTests.cs
+++ languages/csharp/exercises/concept/enums/EnumsTests.cs
@@ -4,33 +4,33 @@ public class LogLineTests
 {
     [Fact]
     public void ParseError() =>
-        Assert.Equal(LogLevel.Error, LogLine.ParseLogLevel("[ERROR]: Disk full"));
+        Assert.Equal(LogLevel.Error, LogLine.ParseLogLevel("[ERR]: Disk full"));
 
     [Fact]
     public void ParseWarning() =>
-        Assert.Equal(LogLevel.Warning, LogLine.ParseLogLevel("[WARNING]: Timezone not set"));
+        Assert.Equal(LogLevel.Warning, LogLine.ParseLogLevel("[WRN]: Timezone not set"));
 
     [Fact]
     public void ParseInfo() =>
-        Assert.Equal(LogLevel.Info, LogLine.ParseLogLevel("[INFO]: Timezone changed"));
+        Assert.Equal(LogLevel.Info, LogLine.ParseLogLevel("[INF]: Timezone changed"));
 
     [Fact]
     public void ParseUnknown() =>
-        Assert.Equal(LogLevel.Unknown, LogLine.ParseLogLevel("[FATAL]: Crash!"));
+        Assert.Equal(LogLevel.Unknown, LogLine.ParseLogLevel("[XYZ]: Crash!"));
 
     [Fact]
     public void OutputForShortLogForError() =>
-        Assert.Equal("4:Stack overflow", LogLine.OutputForShortLog(LogLevel.Error, "Stack overflow"));
+        Assert.Equal("6:Stack overflow", LogLine.OutputForShortLog(LogLevel.Error, "Stack overflow"));
 
     [Fact]
     public void OutputForShortLogForWarning() =>
-        Assert.Equal("2:Unsafe password", LogLine.OutputForShortLog(LogLevel.Warning, "Unsafe password"));
+        Assert.Equal("5:Unsafe password", LogLine.OutputForShortLog(LogLevel.Warning, "Unsafe password"));
 
     [Fact]
     public void OutputForShortLogForInfo() =>
-        Assert.Equal("1:File moved", LogLine.OutputForShortLog(LogLevel.Info, "File moved"));
+        Assert.Equal("4:File moved", LogLine.OutputForShortLog(LogLevel.Info, "File moved"));
 
     [Fact]
     public void OutputForShortLogForUnknown() =>
-        Assert.Equal("0:Something unknown happened", LogLine.OutputForShortLog(LogLevel.Unknown, "Something unknown happened"));
+        Assert.Equal("42:Something unknown happened", LogLine.OutputForShortLog(LogLevel.Unknown, "Something unknown happened"));
 }
