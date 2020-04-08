997c454042ac7d3e67cb7652eb0a7326a8340489
Dario Mancini <dario.mancini@musement.com>
2020-04-08 18:38:57 +0200

    first iteration

--- languages/go/exercises/concept/numbers-floating-point/numbers-floating-point.go
+++ languages/go/exercises/concept/numbers-floating-point/numbers-floating-point.go
@@ -1,61 +1,16 @@
 package numbers_floating_point
 
-import "math"
-
-/*
-//InterestRate
+//InterestRate calculates the interest rate based on the specified balance
 func InterestRate(balance float64) float64 {
 	panic("Please implement the InterestRate method")
 }
 
-//AnnualBalanceUpdate
+//AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate
 func AnnualBalanceUpdate(balance float64) float64 {
 	panic("Please implement the AnnualBalanceUpdate method")
 }
 
-//YearsBeforeDesiredBalance
+//YearsBeforeDesiredBalance calculate the minimum number of years required to reach the desired balance
 func YearsBeforeDesiredBalance(balance float64, targetBalance float64) int {
 	panic("Please implement the YearsBeforeDesiredBalance method")
-}
-*/
-
-//InterestRate
-func InterestRate(balance float64) float64 {
-	if balance < 0.0 {
-		return -3.213
-	}
-
-	if balance < 1000.0 {
-		return 0.5
-	}
-
-	if balance < 5000.0 {
-		return 1.621
-	}
-
-	return 2.475
-
-}
-
-func annualYield(balance float64) float64 {
-	multiplier := InterestRate(balance) / 100
-	return math.Abs(balance) * multiplier
-}
-
-//AnnualBalanceUpdate
-func AnnualBalanceUpdate(balance float64) float64 {
-	return balance + annualYield(balance)
-}
-
-//YearsBeforeDesiredBalance
-func YearsBeforeDesiredBalance(balance float64, targetBalance float64) int {
-	accumulatingBalance := balance
-	years := 0
-
-	for accumulatingBalance < targetBalance {
-		accumulatingBalance = AnnualBalanceUpdate(accumulatingBalance)
-		years++
-	}
-
-	return years
 }
--- languages/go/exercises/concept/numbers-floating-point/numbers-floating-point_test.go
+++ languages/go/exercises/concept/numbers-floating-point/numbers-floating-point_test.go
@@ -4,6 +4,64 @@ import (
 	"testing"
 )
 
+func TestAnnualBalanceUpdate(t *testing.T) {
+	tests := []struct {
+		name    string
+		balance float64
+		want    float64
+	}{
+		{
+			name:    "AnnualBalanceUpdateForEmptyStartBalance",
+			balance: 0,
+			want:    0,
+		},
+		{
+			name:    "AnnualBalanceUpdateForSmallPositiveStartBalance",
+			balance: 0.000001,
+			want:    0.000001005,
+		},
+		{
+			name:    "AnnualBalanceUpdateForAveragePositiveStartBalance",
+			balance: 1000,
+			want:    1016.21,
+		},
+		{ //test case fails in Golang with float64
+			name:    "AnnualBalanceUpdateForLargePositiveStartBalance",
+			balance: 1000.0001,
+			want:    1016.210101621,
+		},
+		{
+			name:    "AnnualBalanceUpdateForHugePositiveStartBalance",
+			balance: 898124017.826243404425,
+			want:    920352587.26744292868451875,
+		},
+		{
+			name:    "AnnualBalanceUpdateForSmallNegativeStartBalance",
+			balance: -0.123,
+			want:    -0.12695199,
+		},
+		{
+			name:    "AnnualBalanceUpdateForLargeNegativeStartBalance",
+			balance: -152964.231,
+			want:    -157878.97174203,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := AnnualBalanceUpdate(tt.balance)
+			if got != tt.want {
+				t.Errorf(
+					"AnnualBalanceUpdate(%f) = %f, want %f",
+					tt.balance,
+					got,
+					tt.want,
+				)
+			}
+		})
+	}
+}
+
 func TestInterestRate(t *testing.T) {
 	tests := []struct {
 		name    string
@@ -84,6 +142,55 @@ func TestInterestRate(t *testing.T) {
 				t.Errorf(
 					"InterestRate(%f) = %f, want %f",
 					tt.balance,
+					got,
+					tt.want,
+				)
+			}
+		})
+	}
+}
+
+func TestYearsBeforeDesiredBalance(t *testing.T) {
+	tests := []struct {
+		name          string
+		balance       float64
+		targetBalance float64
+		want          int
+	}{
+		{
+			name:          "YearsBeforeDesiredBalanceForSmallStartBalance",
+			balance:       100,
+			targetBalance: 125.80,
+			want:          47,
+		},
+		{
+			name:          "YearsBeforeDesiredBalanceForAverageStartBalance",
+			balance:       1000,
+			targetBalance: 1100,
+			want:          6,
+		},
+		{
+			name:          "YearsBeforeDesiredBalanceForLargeStartBalance",
+			balance:       8080.80,
+			targetBalance: 9090.90,
+			want:          5,
+		},
+		{
+			name:          "YearsBeforeDesiredBalanceForLargeDifferentBetweenStartAndTargetBalance",
+			balance:       2345.67,
+			targetBalance: 12345.6789,
+			want:          85,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := YearsBeforeDesiredBalance(tt.balance, tt.targetBalance)
+			if got != tt.want {
+				t.Errorf(
+					"YearsBeforeDesiredBalance(%f, %f) = %d, want %d",
+					tt.balance,
+					tt.targetBalance,
 					got,
 					tt.want,
 				)
