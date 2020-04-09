653fd9848aaa96754afb291f458963e62635b7ec
Erik Schierboom <erik_schierboom@hotmail.com>
2020-04-09 12:07:42 +0200

    [F#] Add equality operators to numbers exercise

--- languages/fsharp/exercises/concept/numbers/NumbersTests.fs
+++ languages/fsharp/exercises/concept/numbers/NumbersTests.fs
@@ -18,7 +18,10 @@ let ``Production rate per hour for speed 4``() = productionRatePerHour 4 |> shou
 let ``Production rate per hour for speed 7``() = productionRatePerHour 7 |> should equal 1392.3
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``Production rate per hour for speed 9``() = productionRatePerHour 9 |> should equal 1531.53
+let ``Production rate per hour for speed 9``() = productionRatePerHour 9 |> should equal 1591.2
+
+[<Fact(Skip = "Remove this Skip property to run this test")>]
+let ``Production rate per hour for speed 10``() = productionRatePerHour 10 |> should equal 1701.7
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Working items per minute for speed 0``() = workingItemsPerMinute 0 |> should equal 0
@@ -31,6 +34,9 @@ let ``Working items per minute for speed 5``() = workingItemsPerMinute 5 |> shou
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Working items per minute for speed 8``() = workingItemsPerMinute 8 |> should equal 26
+
+[<Fact(Skip = "Remove this Skip property to run this test")>]
+let ``Working items per minute for speed 9``() = workingItemsPerMinute 9 |> should equal 26
 
 [<Fact(Skip = "Remove this Skip property to run this test")>]
 let ``Working items per minute for speed 10``() = workingItemsPerMinute 10 |> should equal 28
