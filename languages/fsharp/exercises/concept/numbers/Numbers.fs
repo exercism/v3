09ff1153abb839c3a653a9b7f59d5013bbe745ba
Erik Schierboom <erik_schierboom@hotmail.com>
2020-04-14 17:13:01 +0200

    [F#] Format code

--- languages/fsharp/exercises/concept/numbers/Numbers.fs
+++ languages/fsharp/exercises/concept/numbers/Numbers.fs
@@ -1,7 +1,5 @@
 module Numbers
 
-let productionRatePerHour (speed: int): double = 
-    failwith "Please implement the 'productionRatePerHour' function"
+let productionRatePerHour (speed: int): double = failwith "Please implement the 'productionRatePerHour' function"
 
-let workingItemsPerMinute (speed: int): int = 
-    failwith "Please implement the 'workingItemsPerMinute' function"
+let workingItemsPerMinute (speed: int): int = failwith "Please implement the 'workingItemsPerMinute' function"
