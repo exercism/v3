00aaaf3bd4c9f7045b0ebe2c474437c9db3384fd
Erik Schierboom <erik_schierboom@hotmail.com>
2020-04-14 15:40:25 +0200

    [C#] Add script to format code

---
+++ languages/csharp/format.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+    Format the code of an exercise.
+.DESCRIPTION
+    Format the code. of an exercise
+.PARAMETER Slug
+    The slug of the exercise to format (optional)
+.EXAMPLE
+    The example below will format the code of the "basics" exercise
+    PS C:\> ./format.ps1 basics
+#>
+
+param (
+    [Parameter(Position = 0, Mandatory = $true)]
+    [string]$Slug
+)
+
+dotnet tool restore
+
+$conceptExerciseDir = Join-Path "exercises" "concept" $slug
+$practiceExerciseDIr = Join-Path "exercises" "practice" $slug
+
+if (Test-Path $conceptExerciseDir -PathType Container) {
+    dotnet format -f $conceptExerciseDir
+} elseif (Test-Path $practiceExerciseDIr -PathType Container) {
+    dotnet format -f $practiceExerciseDIr
+} else {
+    Write-Error "Unknown slug"
+}
+
+
