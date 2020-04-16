549a18505f467d088975b3c525b288baad0f7b25
Adam Morris <amorris2003@gmail.com>
2020-04-16 12:27:09 +1000

    canSpy tests

--- languages/purescript/exercises/concept/booleans/src/Booleans.purs
+++ languages/purescript/exercises/concept/booleans/src/Booleans.purs
@@ -1,3 +1,9 @@
 module Booleans where
 
-number = 3
+import Prelude
+
+canExecuteFastAttack :: Boolean -> Boolean
+canExecuteFastAttack knightIsAwake = not knightIsAwake
+
+canSpy :: Boolean -> Boolean -> Boolean -> Boolean
+canSpy knightIsAwake archerIsAwake prisonerIsAwake = knightIsAwake || archerIsAwake || prisonerIsAwake
--- languages/purescript/exercises/concept/booleans/test/Main.purs
+++ languages/purescript/exercises/concept/booleans/test/Main.purs
@@ -2,16 +2,24 @@ module Test.Main where
 
 import Prelude
 
-import Booleans
-import Effect
-import Effect.Aff
-import Test.Spec
-import Test.Spec.Assertions
-import Test.Spec.Reporter.Console
-import Test.Spec.Runner
+import Booleans (canExecuteFastAttack, canSpy)
+import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
-  describe "test" do
-    it "test" do
-      number `shouldEqual` 3
+  describe "Can fast attack" do
+    it "Knight is awake" $ canExecuteFastAttack true `shouldEqual` false
+    it "Knight is asleep" $ canExecuteFastAttack false `shouldEqual` true
+  describe "Can spy" do
+      it "Prisoner is awake" $ canSpy false false true `shouldEqual` true
+      it "Archer is awake" $ canSpy false true false `shouldEqual` true
+      it "Knight is awake" $ canSpy true false false `shouldEqual` true
+      it "Knight and prisoner are awake" $ canSpy true false true `shouldEqual` true
+      it "Knight and archer are awake" $ canSpy true true false `shouldEqual` true
+      it "Everyone is awake" $ canSpy true true true `shouldEqual` true
+      it "Everyone is asleep" $ canSpy false false false `shouldEqual` false
