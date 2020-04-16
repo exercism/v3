7d943c4411d0c19e58fa71d10832722c98136164
Adam Morris <amorris2003@gmail.com>
2020-04-16 13:15:18 +1000

    all tests done

--- languages/purescript/exercises/concept/booleans/src/Booleans.purs
+++ languages/purescript/exercises/concept/booleans/src/Booleans.purs
@@ -7,3 +7,9 @@ canExecuteFastAttack knightIsAwake = not knightIsAwake
 
 canSpy :: Boolean -> Boolean -> Boolean -> Boolean
 canSpy knightIsAwake archerIsAwake prisonerIsAwake = knightIsAwake || archerIsAwake || prisonerIsAwake
+
+canSignalPrisoner :: Boolean -> Boolean -> Boolean
+canSignalPrisoner archerIsAwake prisonerIsAwake = not archerIsAwake && prisonerIsAwake
+
+canFreePrisoner :: Boolean -> Boolean -> Boolean -> Boolean -> Boolean
+canFreePrisoner knightIsAwake archerIsAwake prisonerIsAwake petDogIsPresent = not knightIsAwake && not archerIsAwake && prisonerIsAwake || petDogIsPresent && not archerIsAwake
--- languages/purescript/exercises/concept/booleans/test/Main.purs
+++ languages/purescript/exercises/concept/booleans/test/Main.purs
@@ -2,7 +2,7 @@ module Test.Main where
 
 import Prelude
 
-import Booleans (canExecuteFastAttack, canSpy)
+import Booleans (canExecuteFastAttack, canSpy, canSignalPrisoner, canFreePrisoner)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.Spec (describe, it)
@@ -16,10 +16,32 @@ main = launchAff_ $ runSpec [consoleReporter] do
     it "Knight is awake" $ canExecuteFastAttack true `shouldEqual` false
     it "Knight is asleep" $ canExecuteFastAttack false `shouldEqual` true
   describe "Can spy" do
-      it "Prisoner is awake" $ canSpy false false true `shouldEqual` true
-      it "Archer is awake" $ canSpy false true false `shouldEqual` true
-      it "Knight is awake" $ canSpy true false false `shouldEqual` true
-      it "Knight and prisoner are awake" $ canSpy true false true `shouldEqual` true
-      it "Knight and archer are awake" $ canSpy true true false `shouldEqual` true
-      it "Everyone is awake" $ canSpy true true true `shouldEqual` true
-      it "Everyone is asleep" $ canSpy false false false `shouldEqual` false
+    it "Knight is asleep, archer is asleep, prisoner is awake" $ canSpy false false true `shouldEqual` true
+    it "Knight is asleep, archer is awake, prisoner is asleep" $ canSpy false true false `shouldEqual` true
+    it "Knight is awake, archer is asleep, prisoner is asleep" $ canSpy true false false `shouldEqual` true
+    it "Knight is awake, archer is asleep, prisoner is awake" $ canSpy true false true `shouldEqual` true
+    it "Knight is awake, archer is awake, prisoner is asleep" $ canSpy true true false `shouldEqual` true
+    it "Knight is awake, archer is awake, prisoner is awake" $ canSpy true true true `shouldEqual` true
+    it "Knight is asleep, archer is asleep, prisoner is asleep" $ canSpy false false false `shouldEqual` false
+  describe "Can signal prisoner" do
+    it "Archer is asleep, prisoner is asleep" $ canSignalPrisoner false false `shouldEqual` false
+    it "Archer is asleep, prisoner is awake" $ canSignalPrisoner false true `shouldEqual` true
+    it "Archer is awake, prisoner is asleep" $ canSignalPrisoner true false `shouldEqual` false
+    it "Archer is awake, prisoner is awake" $ canSignalPrisoner true true `shouldEqual` false
+  describe "Can free prisoner" do
+    it "Knight is asleep, archer is asleep, prisoner is asleep, pet dog is not present" $ canFreePrisoner false false false false `shouldEqual` false
+    it "Knight is asleep, archer is asleep, prisoner is asleep, pet dog is present" $ canFreePrisoner false false false true `shouldEqual` true
+    it "Knight is asleep, archer is asleep, prisoner is awake, pet dog is not present" $ canFreePrisoner false false true false `shouldEqual` true
+    it "Knight is asleep, archer is asleep, prisoner is awake, pet dog is present" $ canFreePrisoner false false true true `shouldEqual` true
+    it "Knight is asleep, archer is awake, prisoner is asleep, pet dog is not present" $ canFreePrisoner false true false false `shouldEqual` false
+    it "Knight is asleep, archer is awake, prisoner is asleep, pet dog is present" $ canFreePrisoner false true false true `shouldEqual` false
+    it "Knight is asleep, archer is awake, prisoner is awake, pet dog is not present" $ canFreePrisoner false true true false `shouldEqual` false
+    it "Knight is asleep, archer is awake, prisoner is awake, pet dog is present" $ canFreePrisoner false true true true `shouldEqual` false
+    it "Knight is awake, archer is asleep, prisoner is asleep, pet dog is not present" $ canFreePrisoner true false false false `shouldEqual` false
+    it "Knight is awake, archer is asleep, prisoner is asleep, pet dog is present" $ canFreePrisoner true false false true `shouldEqual` true
+    it "Knight is awake, archer is asleep, prisoner is awake, pet dog is not present" $ canFreePrisoner true false true false `shouldEqual` false
+    it "Knight is awake, archer is asleep, prisoner is awake, pet dog is present" $ canFreePrisoner true false true true `shouldEqual` true
+    it "Knight is awake, archer is awake, prisoner is asleep, pet dog is not present" $ canFreePrisoner true true false false `shouldEqual` false
+    it "Knight is awake, archer is awake, prisoner is asleep, pet dog is present" $ canFreePrisoner true true false true `shouldEqual` false
+    it "Knight is awake, archer is awake, prisoner is awake, pet dog is not present" $ canFreePrisoner true true true false `shouldEqual` false
+    it "Knight is awake, archer is awake, prisoner is awake, pet dog is present" $ canFreePrisoner true true true true `shouldEqual` false
