2af4e4a21fa54aa15628e5485211d078563716bc
Adam Morris <amorris2003@gmail.com>
2020-04-16 19:28:38 +1000

    port instructions.md

--- languages/purescript/exercises/concept/basics/test/Main.purs
+++ languages/purescript/exercises/concept/basics/test/Main.purs
@@ -2,10 +2,15 @@ module Test.Main where
 
 import Prelude
 
+import Booleans (canExecuteFastAttack, canSpy, canSignalPrisoner, canFreePrisoner)
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Aff (launchAff_)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main = launchAff_ $ runSpec [consoleReporter] do
+  describe "Can fast attack" do
+    it "Knight is awake" $ canExecuteFastAttack true `shouldEqual` false
