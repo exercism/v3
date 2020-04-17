f24c9698ea0842207e4e0a702e7c3668a9756f4b
Sascha Mann <git@mail.saschamann.eu>
2020-04-17 21:41:01 +0200

    Add tests

--- languages/julia/exercises/concept/composite-types/runtests.jl
+++ languages/julia/exercises/concept/composite-types/runtests.jl
@@ -2,3 +2,22 @@ using Test
 
 include("robots.jl")
 
+using Test
+
+include("chesspieces.jl")
+
+@testset "subtype" begin
+    @test Pawn <: Chesspiece
+end
+
+@testset "colours" begin
+    p = Pawn(:black)
+    @test colour(p) == :black
+    
+    p = Pawn(:white)
+    @test colour(p) == :white
+end
+
+@testset "colour validation" begin
+    @test_throws DomainError Pawn(:yellow)
+end
