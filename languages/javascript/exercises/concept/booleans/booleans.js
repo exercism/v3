1cf7b6941e514be4b7ab9321462298b58888dc26
Rishi Kothari <itsrishikothari@gmail.com>
2020-04-22 15:25:43 -0400

    Added documentation for canSpy function

--- languages/javascript/exercises/concept/booleans/booleans.js
+++ languages/javascript/exercises/concept/booleans/booleans.js
@@ -9,6 +9,15 @@ export function canExecuteFastAttack(knightIsAwake) {
   throw new Error('Implement the canExecuteFastAttack function')
 }
 
+/**
+ * You can only spy if someone is awake; you wouldn't want to go through all the trouble just to find out nothing's going to happen, would you?
+ *
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ * 
+ * @returns {boolean} Whether or not you can spy on someone.
+ */
 export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
   throw new Error('Implement the canSpy function')
 }
