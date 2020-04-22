59746f3368c1b4f854b9a02b159aa7e02fb10581
Rishi Kothari <itsrishikothari@gmail.com>
2020-04-22 15:48:19 -0400

    add documentation for last two functions

--- languages/javascript/exercises/concept/booleans/booleans.js
+++ languages/javascript/exercises/concept/booleans/booleans.js
@@ -22,10 +22,24 @@ export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
   throw new Error('Implement the canSpy function')
 }
 
+/**
+ * If you signal to the prisoner while the archer is awake, the archer will catch you! However, the prisoner also has to be awake for them to receive your signal!
+ *
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ * 
+ * @returns {boolean} Whether or not you can send a signal to the prisoner.
+ */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
   throw new Error('Implement the canSignalPrisoner function')
 }
 
+/**
+ * @param {boolean} knightIsAwake
+ * @param {boolean} archerIsAwake
+ * @param {boolean} prisonerIsAwake
+ * @param {boolean} petDogIsPresent
+ */
 export function canFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent) {
   throw new Error('Implement the canFreePrisoner function')
 }
