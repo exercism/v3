5bf79bf2dcc758ba0b5cab28b8677ca80af9be53
Tim Austin <tim@neenjaw.com>
2020-04-23 15:04:52 -0600

    exercise skeleton

---
+++ languages/elixir/exercises/concept/maps/lib/high_score.ex
@@ -0,0 +1,33 @@
+defmodule HighScore do
+  @initial_score 0
+
+  def new(), do: %{}
+
+  def add_player(scores, name) do
+    Map.put(scores, name, @initial_score)
+  end
+
+  def remove_player(scores, name) do
+    Map.delete(scores, name)
+  end
+
+  def update(scores, name, score) do
+    Map.update(scores, name, @initial_score, &(&1 + score))
+  end
+
+  def reset(scores, name) do
+    Map.put(scores, name, @initial_score)
+  end
+
+  def ordered_players(scores) do
+    scores
+    |> Map.keys()
+    |> Enum.sort_by(&String.downcase/1)
+  end
+
+  def ordered_scores(scores) do
+    scores
+    |> Map.to_list()
+    |> Enum.sort_by(&Kernel.elem(&1, 1))
+  end
+end
---
+++ languages/elixir/exercises/concept/maps/test/high_score_test.exs
@@ -0,0 +1,8 @@
+defmodule HighScoreTest do
+  use ExUnit.Case
+  doctest HighScore
+
+  test "greets the world" do
+    assert HighScore.hello() == :world
+  end
+end
