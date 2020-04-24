26928491fb94bde21c69ee966a5ff536d04d4ad2
Erik Schierboom <erik_schierboom@hotmail.com>
2020-04-24 17:11:56 +0200

    [C#] Add method suffix to stub messages

--- languages/csharp/exercises/concept/nullability/Nullability.cs
+++ languages/csharp/exercises/concept/nullability/Nullability.cs
@@ -4,13 +4,13 @@ public static class Badge
 {
     public static string Label(int? id, string name, string? department)
     {
-        throw new NotImplementedException("Please implement the Badge.Label method");
+        throw new NotImplementedException("Please implement the Badge.Label() method");
     }
 
     public static string PrintLabel(string? prefix,
                                     string label,
                                     int maximumWidth)
     {
-        throw new NotImplementedException("Please implement the Badge.PrintLabel method");
+        throw new NotImplementedException("Please implement the Badge.PrintLabel() method");
     }
 }
