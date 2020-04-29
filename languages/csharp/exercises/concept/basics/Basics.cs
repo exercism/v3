cfd3c3a3804eb2ddf2652c5413e63eac4fb28115
Erik Schierboom <erik_schierboom@hotmail.com>
2020-04-29 16:50:45 +0200

    [C#] Revert

--- languages/csharp/exercises/concept/basics/Basics.cs
+++ languages/csharp/exercises/concept/basics/Basics.cs
@@ -1,22 +1,10 @@
 class Lasagna
 {
-    public int ExpectedMinutesInOven()
-    {
-        return 40;
-    }
+    // TODO: define the 'ExpectedMinutesInOven' method
 
-    public int RemainingMinutesInOven(int actualMinutesInOven)
-    {
-        return ExpectedMinutesInOven() - actualMinutesInOven;
-    }
+    // TODO: define the 'RemainingMinutesInOven' method
 
-    public int PreparationTimeInMinutes(int numberOfLayers)
-    {
-        return numberOfLayers * 2;
-    }
+    // TODO: define the 'PreparationTimeInMinutes' method
 
-    public int TotalTimeInMinutes(int numberOfLayers, int actualMinutesInOven)
-    {
-        return PreparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
-    }
+    // TODO: define the 'TotalTimeInMinutes' method
 }
