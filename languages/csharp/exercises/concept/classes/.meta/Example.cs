ae78ee68290d99a5df7b6917ec28b21fcc18dcb8
Erik Schierboom <erik_schierboom@hotmail.com>
2020-04-30 14:48:23 +0200

    [C#] Improve example

--- languages/csharp/exercises/concept/classes/.meta/Example.cs
+++ languages/csharp/exercises/concept/classes/.meta/Example.cs
@@ -1,30 +1,30 @@
 class RemoteControlCar
 {
-    private int battery = 100;
-    private int distance = 0;
+    private int batteryPercentage = 100;
+    private int distanceDrivenInMeters = 0;
 
     public void Drive()
     {
-        if (battery > 0)
+        if (batteryPercentage > 0)
         {
-            battery--;
-            distance += 20;
+            batteryPercentage--;
+            distanceDrivenInMeters += 20;
         }
     }
 
     public string DistanceDisplay()
     {
-        return $"Driven {distance} meters";
+        return $"Driven {distanceDrivenInMeters} meters";
     }
 
     public string BatteryDisplay()
     {
-        if (battery == 0)
+        if (batteryPercentage == 0)
         {
             return "Battery empty";
         }
 
-        return $"Battery at {battery}%";
+        return $"Battery at {batteryPercentage}%";
     }
 
     public static RemoteControlCar Buy()
