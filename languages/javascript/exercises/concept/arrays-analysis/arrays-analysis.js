b18008b2b5d86fee261d31c4dfe43b561a020045
peterchu999 <peter.andrew987@gmail.com>
2020-04-30 22:51:55 +0700

    new concept arrays-analysis exercise #1370

---
+++ languages/javascript/exercises/concept/arrays-analysis/arrays-analysis.js
@@ -0,0 +1,67 @@
+/**
+ * Get array index with the value of card
+ *
+ * @param {number[]} array
+ * @param {number} card
+ *
+ * @returns {number} Array index with the value of card
+ */
+export function getCardIndex(array, card) {
+  throw new Error('Implement the getCardIndex function')
+}
+
+/**
+ * Check if the array contains cardGiven
+ *
+ * @param {number[]} array
+ * @param {number} cardGiven
+ *
+ * @returns {boolean} Whether contains a cardGiven
+ */
+export function isStackIncludes(array, cardGiven) {
+  throw new Error('Implement the isStackIncludes function')
+}
+
+/**
+ * Check if every value in array was even
+ *
+ * @param {number[]} array
+ *
+ * @returns {boolean} Whether every value in array was even
+ */
+export function isEvenStack(array) {
+  throw new Error('Implement the isEvenStack function')
+}
+
+/**
+ * Check if at least one odd value card
+ *
+ * @param {number[]} array
+ *
+ * @returns {boolean} Whether array contains odd card
+ */
+export function isStackContainsOdd(array) {
+  throw new Error('Implement the isStackContainsOdd function')
+}
+
+/**
+ * Get the first odd value card from array
+ *
+ * @param {number[]} array
+ *
+ * @returns {number} First odd value card
+ */
+export function getFirstOddCard(array) {
+  throw new Error('Implement the getFirstOddCard function')
+}
+
+/**
+ * Get the index of first even value card from array
+ *
+ * @param {number[]} array
+ *
+ * @returns {number} First even value card index
+ */
+export function getFirstEvenCardIndex(array) {
+  throw new Error('Implement the getFirstEvenCardIndex function')
+}
