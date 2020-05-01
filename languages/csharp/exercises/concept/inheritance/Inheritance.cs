92253a2df5bb3311462f7009fa239a5565ef15e6
Erik Schierboom <erik_schierboom@hotmail.com>
2020-05-01 16:27:40 +0200

    [C#] Add inheritance exercise

---
+++ languages/csharp/exercises/concept/inheritance/Inheritance.cs
@@ -0,0 +1,36 @@
+using System;
+
+class RemoteControlCar
+{
+    // TODO: define the 'RemoteControlCar' classes' constructor
+
+    public bool BatteryDrained()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
+    }
+
+    public int DistanceDriven()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
+    }
+
+    public void Drive()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+    }
+
+    public static RemoteControlCar TopModel()
+    {
+        throw new NotImplementedException("Please implement the (static) RemoteControlCar.TopModel() method");
+    }
+}
+
+class RaceTrack
+{
+    // TODO: define the 'RaceTrack' classes' constructor
+
+    public bool CarCanFinish(RemoteControlCar car)
+    {
+        throw new NotImplementedException("Please implement the RaceTrack.CarCanFinish() method");
+    }
+}
