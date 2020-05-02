67e8028d749a78ec8769884b2ffab51b85c9d870
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-02 12:57:09 -0400

    add constructor test for wand

--- languages/javascript/exercises/concept/classes-intro/classes-intro.spec.js
+++ languages/javascript/exercises/concept/classes-intro/classes-intro.spec.js
@@ -1,7 +1,18 @@
+import { Wizard, Wand } from "./classes-intro";
+
 describe("class-intro", () => {
     describe("canConstructWand", () => {
         it("can construct with a variety of options", () => {
-            
+            const initialize_array = [
+                ["Dragon Heartstring", "Oak"]
+            ]
+
+            initialize_array.forEach(initializer => {
+                const wand = new Wand(initializer[0], initializer[1]);
+
+                expect(wand.core).toBe(initializer[0]);
+                expect(wand.material).toBe(initializer[1])
+            })
         })
     })  
 })
