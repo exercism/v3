a5824236304f66fb9a489e2a26a1da36a8704c1c
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-02 13:13:15 -0400

    Add in spell and info tests

--- languages/javascript/exercises/concept/classes-intro/classes-intro.spec.js
+++ languages/javascript/exercises/concept/classes-intro/classes-intro.spec.js
@@ -1,17 +1,33 @@
 import { Wizard, Wand } from "./classes-intro";
 
-describe("class-intro", () => {
-    describe("canConstructWand", () => {
-        it("can construct with a variety of options", () => {
-            const initialize_array = [
-                ["Dragon Heartstring", "Oak"]
-            ]
+const initialize_arr = [
+    ["Dragon Heartstring", "Oak"]
+]
 
-            initialize_array.forEach(initializer => {
+describe("class-intro", () => {
+    describe("wandTests", () => {
+        it("can construct with a variety of options", () => {
+
+            initialize_arr.forEach(initializer => {
                 const wand = new Wand(initializer[0], initializer[1]);
 
                 expect(wand.core).toBe(initializer[0]);
                 expect(wand.material).toBe(initializer[1])
+            })
+        })
+        it("can do spells", () => {
+            const wand = new Wand(...initialize_arr[0]);
+
+            const spell = "expelliarmus";
+
+            expect(wand.useSpell(spell)).toBe(`Used spell ${spell}.`)
+        })
+
+        it("can display information based off of its properties", () => {
+
+            initialize_arr.forEach(([core, material]) => {
+                const wand = new Wand(core, material);
+                expect(wand.info()).toBe(`This wand is made of ${material}, and has a core of ${core}.`)
             })
         })
     })  
