1155897b85172aa409d2c86877bb9938b43f9293
Erik Schierboom <erik_schierboom@hotmail.com>
2020-05-03 09:07:10 +0200

    [C#] Work

--- languages/csharp/exercises/concept/inheritance/Inheritance.cs
+++ languages/csharp/exercises/concept/inheritance/Inheritance.cs
@@ -1,36 +1,21 @@
 using System;
 
-class RemoteControlCar
+abstract class ScoreReport
 {
-    // TODO: define the 'RemoteControlCar' classes' constructor
-
-    public bool BatteryDrained()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDrained() method");
-    }
-
-    public int DistanceDriven()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDriven() method");
-    }
-
-    public void Drive()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
-    }
-
-    public static RemoteControlCar TopModel()
-    {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.TopModel() method");
-    }
+    
 }
 
-class RaceTrack
+class TickerReport : ScoreReport
 {
-    // TODO: define the 'RaceTrack' classes' constructor
+    
+}
 
-    public bool CarCanFinish(RemoteControlCar car)
-    {
-        throw new NotImplementedException("Please implement the RaceTrack.CarCanFinish() method");
-    }
+class ArticleReport : ScoreReport
+{
+    
+}
+
+class NewspaperReport : ScoreReport
+{
+    
 }
