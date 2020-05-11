8c66fc11319b5907796ebfa5e4b3abd73929d29b
TehSphinX <tehsphinx@web.de>
2020-05-11 19:18:33 +0200

    transfer basics exercise from C# to Go; TODO: tests

---
+++ languages/go/exercises/concept/basics/basics.go
@@ -0,0 +1,9 @@
+package basics
+
+// TODO: define the 'ExpectedMinutesInOven()' function
+
+// TODO: define the 'RemainingMinutesInOven()' function
+
+// TODO: define the 'PreparationTimeInMinutes()' function
+
+// TODO: define the 'TotalTimeInMinutes()' function
