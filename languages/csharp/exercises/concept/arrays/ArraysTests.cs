1b5625ae0b82ddbd5a80f9b339b3cf404cc60247
Erik Schierboom <erik_schierboom@hotmail.com>
2020-05-19 09:55:16 +0200

    [C#] Fix test names

--- languages/csharp/exercises/concept/arrays/ArraysTests.cs
+++ languages/csharp/exercises/concept/arrays/ArraysTests.cs
@@ -2,14 +2,14 @@ using Xunit;
 
 public class BirdCountTests
 {
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Last_week()
     {
         Assert.Equal(new int[] { 0, 2, 5, 3, 7, 8, 4 }, BirdCount.LastWeek());
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Yesterday_for_disappointing_week()
+    public void Today_for_disappointing_day()
     {
         var counts = new int[] { 0, 0, 1, 0, 0, 1, 0 };
         var birdCount = new BirdCount(counts);
@@ -17,7 +17,7 @@ public class BirdCountTests
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Yesterday_days_for_busy_week()
+    public void Today_for_busy_day()
     {
         var counts = new int[] { 8, 8, 9, 5, 4, 7, 10 };
         var birdCount = new BirdCount(counts);
@@ -25,7 +25,7 @@ public class BirdCountTests
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void HasDayWithoutBirdsWithDayWithoutBirds()
+    public void Has_day_without_birds_with_day_without_birds()
     {
         var counts = new int[] { 5, 5, 4, 0, 7, 6, 7 };
         var birdCount = new BirdCount(counts);
@@ -33,7 +33,7 @@ public class BirdCountTests
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void HasDayWithoutBirdsWithNoDayWithoutBirds()
+    public void Has_day_without_birds_with_no_day_without_birds()
     {
         var counts = new int[] { 4, 5, 9, 10, 9, 4, 3 };
         var birdCount = new BirdCount(counts);
@@ -41,7 +41,7 @@ public class BirdCountTests
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void IncrementTodaysCountWithNoPreviousVisits()
+    public void Increment_todays_count_with_no_previous_visits()
     {
         var counts = new int[] { 0, 0, 0, 4, 2, 3, 0 };
         var birdCount = new BirdCount(counts);
@@ -50,7 +50,7 @@ public class BirdCountTests
     }
 
     [Fact(Skip = "Remove this Skip property to run this test")]
-    public void IncrementTodaysCountWithMultiplePreviousVisits()
+    public void Increment_todays_count_with_multiple_previous_visits()
     {
         var counts = new int[] { 8, 8, 9, 2, 1, 6, 4 };
         var birdCount = new BirdCount(counts);
@@ -58,7 +58,7 @@ public class BirdCountTests
         Assert.Equal(5, birdCount.Today());
     }
 
-    [Fact]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void Total_for_disappointing_week()
     {
         var counts = new int[] { 0, 0, 1, 0, 0, 1, 0 };
