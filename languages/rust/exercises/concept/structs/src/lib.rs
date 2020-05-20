6ee31c9cd1efe487f73c1f7ded0b8619a7075cd0
Sean Chen <skypemaster007@gmail.com>
2020-05-19 17:48:15 -0700

    Uncomment the correct skeleton code

--- languages/rust/exercises/concept/structs/src/lib.rs
+++ languages/rust/exercises/concept/structs/src/lib.rs
@@ -1,69 +1,32 @@
-// #[derive(Clone, Debug)]
-// pub struct User {
-//     name: String,
-//     age: u32,
-//     weight: f32,
-// }
-
-// impl User {
-//     pub fn new(name: String, age: u32, weight: f32) -> Self {
-//         unimplemented!()
-//     }
-
-//     pub fn name(&self) -> &str {
-//         unimplemented!()
-//     }
-
-//     pub fn age(&self) -> u32 {
-//         unimplemented!()
-//     }
-
-//     pub fn weight(&self) -> f32 {
-//         unimplemented!()
-//     }
-
-//     pub fn set_age(&mut self, new_age: u32) {
-//         unimplemented!()
-//     }
-
-//     pub fn set_weight(&mut self, new_weight: f32) {
-//         unimplemented!()
-//     }
-// }
-
 #[derive(Clone, Debug)]
 pub struct User {
-   name: String,
-   age: u32,
-   weight: f32, 
+    name: String,
+    age: u32,
+    weight: f32,
 }
 
 impl User {
     pub fn new(name: String, age: u32, weight: f32) -> Self {
-        User {
-            name,
-            age,
-            weight,
-        }
+        unimplemented!()
     }
 
     pub fn name(&self) -> &str {
-        self.name.as_str()
+        unimplemented!()
     }
 
     pub fn age(&self) -> u32 {
-        self.age
+        unimplemented!()
     }
 
     pub fn weight(&self) -> f32 {
-        self.weight
+        unimplemented!()
     }
 
     pub fn set_age(&mut self, new_age: u32) {
-        self.age = new_age;
+        unimplemented!()
     }
 
     pub fn set_weight(&mut self, new_weight: f32) {
-        self.weight = new_weight;
+        unimplemented!()
     }
 }
