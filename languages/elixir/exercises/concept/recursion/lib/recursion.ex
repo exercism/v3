42b7f2fc2d9bcc06e6f078a83e6251e60ddae2cc
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-05-25 17:14:24 +0200

    Raise errors in stubs

--- languages/elixir/exercises/concept/recursion/lib/recursion.ex
+++ languages/elixir/exercises/concept/recursion/lib/recursion.ex
@@ -1,21 +1,21 @@
 defmodule BirdCount do
   def today(list) do
-    "Please implement the today/1 function"
+    raise "Please implement the today/1 function"
   end
 
   def increment_day_count(list) do
-    "Please implement the increment_day_count/1 function"
+    raise "Please implement the increment_day_count/1 function"
   end
 
   def has_day_without_birds?(list) do
-    "Please implement the has_day_without_birds?/1 function"
+    raise "Please implement the has_day_without_birds?/1 function"
   end
 
   def total(list) do
-    "Please implement the total/1 function"
+    raise "Please implement the total/1 function"
   end
 
   def busy_days(list) do
-    "Please implement the busy_days/1 function"
+    raise "Please implement the busy_days/1 function"
   end
 end
