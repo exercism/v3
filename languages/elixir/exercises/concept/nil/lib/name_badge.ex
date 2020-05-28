4105dd39613d8f35794cbf6adc2a43bb09ed8b52
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-05-28 16:37:08 +0200

    Add stub

--- languages/elixir/exercises/concept/nil/lib/name_badge.ex
+++ languages/elixir/exercises/concept/nil/lib/name_badge.ex
@@ -1,8 +1,5 @@
 defmodule NameBadge do
   def print(id, name, department) do
-    department = if department, do: department, else: "owner"
-    prefix = if id, do: "[#{id}] - ", else: ""
-
-    prefix <> "#{name} - #{String.upcase(department)}"
+    raise "Please implement the print/3 function"
   end
 end
