c3713ee854d460dd44b962968e7b296e1a48879c
Michael Cuffaro <michael@cuffaro.com>
2020-06-01 19:21:08 +0200

    feat: add more functions to sample exercise

--- languages/go/exercises/concept/errors/.meta/sample.go
+++ languages/go/exercises/concept/errors/.meta/sample.go
@@ -6,12 +6,32 @@ import (
 )
 
 const answer = 42
-var ErrCalculation = errors.New("calculation error:")
 
-// AnswerToLife checks if your number is the answer to life
+// ErrCalculation is a general error that can be exported
+var ErrCalculation = errors.New("calculation error")
+
+// ComputerState determines if the deepthought computer is ready for its computation
+func ComputerState(isReady bool) error {
+	if !isReady {
+		return errors.New("computer still booting")
+	}
+	return nil
+}
+
+// AnswerToLife checks if the given number is the answer to life
 func AnswerToLife(number int) (bool, error) {
 	if number != answer {
-		return false, fmt.Errorf("%s wrong answer to life: %s", ErrCalculation.Error(), number)
+		return false, fmt.Errorf("%s: wrong answer to life: %s", ErrCalculation.Error(), number)
 	}
 	return true, nil
+}
+
+// AskComputer checks if number is the answer to life, depending on the state of the computer
+func AskComputer(isReady bool, number int) (bool, error) {
+	err := ComputerState(isReady)
+	if err != nil {
+		return false, fmt.Errorf("proposal %d could not be processed: %s", number, err)
+	}
+	status, err := AnswerToLife(number)
+	return status, err
 }
