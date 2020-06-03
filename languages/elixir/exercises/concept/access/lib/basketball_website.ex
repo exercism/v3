ffb9ffe478692f838c5fcb8f73c2d3c0c9abfe11
Tim Austin <tim@neenjaw.com>
2020-06-03 10:33:30 -0600

    code

--- languages/elixir/exercises/concept/access/lib/basketball_website.ex
+++ languages/elixir/exercises/concept/access/lib/basketball_website.ex
@@ -1,20 +1,9 @@
 defmodule BasketballWebsite do
-  @example1 %{ "coach" => %{ "name" => "Jane" }, "name" => "Hoop Masters" }
-  def p1(), do: ".coach.name"
-  def ex1(), do: @example1
-
-  @example2 %{ "coaches" => [%{ "name" => "Jane" }], "name" => "Hoop Masters" }
-  def p2(), do: ".coaches[0].name"
-  def ex2(), do: @example1
-
-  def extract(data, path) do
-    paths = String.split(path, ".", trim: true)
-    do_extract(data, paths)
+  def extract_from_path(data, path) do
+    raise "Please implement extract_from_path/2"
   end
 
-  def do_extract(nil, _), do: nil
-  def do_extract(data, []), do: data
-  def do_extract(data, [path|next]) do
-    do_extract(data[path], next)
+  def get_in_path(data, path) do
+    raise "Please implement get_in_path/2"
   end
 end
