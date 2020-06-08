7ad4177a7798480edbcdfd71573f424dae0bc1e3
Tim Austin <tim@neenjaw.com>
2020-06-08 10:29:21 -0600

    metaprogramming removed

--- languages/elixir/exercises/concept/bitstrings/.meta/example.ex
+++ languages/elixir/exercises/concept/bitstrings/.meta/example.ex
@@ -6,30 +6,24 @@ defmodule DNA do
 
    | NucleicAcid | Bits |
    | ----------- | ---- |
+   |    ' '      | 0000 |
    |     A       | 0001 |
    |     C       | 0010 |
    |     G       | 0100 |
    |     T       | 1000 |
-   |    " "      | 0000 |
   """
 
-  @pairs [
-    {?\s, 0b0000},
-    {?A, 0b0001},
-    {?C, 0b0010},
-    {?G, 0b0100},
-    {?T, 0b1000},
-  ]
+  def encode_nucleotide(?\s), do: 0b0000
+  def encode_nucleotide(?A), do: 0b0001
+  def encode_nucleotide(?C), do: 0b0010
+  def encode_nucleotide(?G), do: 0b0100
+  def encode_nucleotide(?T), do: 0b1000
 
-  @pairs
-  |> Enum.each(fn {codepoint, code} ->
-    def encode_nucleotide(unquote(codepoint)), do: unquote(code)
-  end)
-
-  @pairs
-  |> Enum.each(fn {codepoint, code} ->
-    def decode_nucleotide(unquote(code)), do: unquote(codepoint)
-  end)
+  def decode_nucleotide(0b0000), do: ?\s
+  def decode_nucleotide(0b0001), do: ?A
+  def decode_nucleotide(0b0010), do: ?C
+  def decode_nucleotide(0b0100), do: ?G
+  def decode_nucleotide(0b1000), do: ?T
 
   def encode(dna) do
     do_encode(dna)
