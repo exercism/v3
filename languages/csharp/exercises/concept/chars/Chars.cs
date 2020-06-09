07ae28abbbf14b5e8407c3459d775917bb9708ac
Mike May <mikemay.mdamay@googlemail.com>
2020-06-09 13:42:36 +0100

    Apply suggestions from code review
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- languages/csharp/exercises/concept/chars/Chars.cs
+++ languages/csharp/exercises/concept/chars/Chars.cs
@@ -2,8 +2,8 @@ using System;
 
 public static class Identifier
 {
-    public static string Clean(string str)
+    public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the Clean() method");
+        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
     }
 }
