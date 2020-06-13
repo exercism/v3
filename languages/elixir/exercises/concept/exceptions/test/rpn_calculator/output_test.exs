19d8f5e6478c6ba5e3566f8daa6c9577c07ed65d
Tim Austin <tim@neenjaw.com>
2020-06-13 00:18:38 -0600

    refactor to simpler test harness

--- languages/elixir/exercises/concept/exceptions/test/rpn_calculator/output_test.exs
+++ languages/elixir/exercises/concept/exceptions/test/rpn_calculator/output_test.exs
@@ -1,124 +1,66 @@
 defmodule RPNCalculator.OutputTest do
   use ExUnit.Case
 
-  setup_all do
-    {:ok, pid} = MockResource.start()
-    [mock_resource: pid]
+  import ExUnit.CaptureIO
+
+  @resource __MODULE__
+  @filename "filename"
+  @bad_filename "bad_filename"
+  @equation "1 1 +"
+
+  def open(filename \\ "<nil>") do
+    send(self(), {:open, filename})
+
+    case filename do
+      @filename -> {:ok, :stdio}
+      @bad_filename -> {:ok, spawn fn -> nil end}
+    end
   end
 
-  setup do
-    on_exit(fn ->
-      MockResource.set_test_failure(false)
-      MockResource.close_all()
-      MockResource.clear_log()
-    end)
+  def close(_) do
+    send(self(), :close)
+    :ok
   end
 
   describe "write/3" do
-    @tag :pending
-    @filename "filename"
-    @equation "1 1 +"
+    test "returns ok tuple `{:ok, <equation>}` if function succeeds" do
+      assert {:ok, @equation} == RPNCalculator.Output.write(@resource, @filename, @equation)
+    end
+
     @use_open_error_message """
-    Use the open/1 function from the `resource` specified in the arguments to open `filename`.
+      Use the open/1 function from the `resource` specified in the arguments to open `filename`.
 
-    E.g.) resource.open(filename)
-    """
-    test "opens the resource" do
-      RPNCalculator.Output.write(MockResource, @filename, @equation)
-
-      with log <- MockResource.get_log(),
-           true <- length(log) > 0,
-           open_entry <- Enum.at(log, 0),
-           true <- String.starts_with?(open_entry, "[OPEN]"),
-           msg <- String.trim_leading(open_entry, "[OPEN]") |> String.trim(),
-           opened_file <- msg |> String.split(":", parts: 2) |> hd(),
-           true <- opened_file == @filename do
-        assert true
-      else
-        _ ->
-          flunk(@use_open_error_message)
-      end
+      E.g.) resource.open(filename)
+      """
+    test "opens resource" do
+      RPNCalculator.Output.write(@resource, @filename, @equation)
+      assert_received {:open, @filename}, @use_open_error_message
     end
 
-    @tag :pending
-    @filename "filename"
-    @equation "1 1 +"
     @use_write_error_message """
-    Use IO.write/2 to write to the opened `filename`.
-    """
-    test "then writes to the resource" do
-      RPNCalculator.Output.write(MockResource, @filename, @equation)
-
-      with log <- MockResource.get_log(),
-           true <- length(log) > 0,
-           msg_entry <- Enum.at(log, 1),
-           true <- String.starts_with?(msg_entry, "[MSG]"),
-           equation <- String.trim_leading(msg_entry, "[MSG]") |> String.trim(),
-           true <- equation == @equation do
-        assert true
-      else
-        _ ->
-          flunk(@use_write_error_message)
-      end
+      Use IO.write/2 to write to the opened `filename`.
+      """
+    test "writes to resource" do
+      assert capture_io(fn -> RPNCalculator.Output.write(@resource, @filename, @equation) end) == "1 1 +", @use_write_error_message
     end
 
-    @tag :pending
-    @filename "filename"
-    @equation "1 1 +"
     @use_close_error_message """
-    Use the close/1 function from the `resource` specified in the arguments to close the opened file handle.
+      Use the close/1 function from the `resource` specified in the arguments to close the opened file handle.
 
-    E.g.) resource.close(filename)
-    """
-    test "then closes the resource" do
-      RPNCalculator.Output.write(MockResource, @filename, @equation)
-
-      with log <- MockResource.get_log(),
-           true <- length(log) == 3,
-           msg_entry <- Enum.at(log, 2),
-           true <- String.starts_with?(msg_entry, "[CLOSE]") do
-        assert true
-      else
-        _ ->
-          flunk(@use_close_error_message)
-      end
+      E.g.) resource.close(filename)
+      """
+    test "closes resource" do
+      RPNCalculator.Output.write(__MODULE__, @filename, @equation)
+      assert_received :close, @use_close_error_message
     end
 
-    @tag :pending
-    @filename "filename"
-    @equation "1 1 +"
-    test "returns error tuple when error raised on write" do
-      MockResource.set_test_failure(true)
-
-      assert {:error, "Unable to write to resource"} =
-               RPNCalculator.Output.write(MockResource, @filename, @equation)
+    test "rescues and returns error tuple `{:error, \"Unable to write to resource\"}` from raised error" do
+      assert {:error, "Unable to write to resource"} == RPNCalculator.Output.write(@resource, @bad_filename, @equation)
     end
 
-    @tag :pending
-    @filename "filename"
-    @equation "1 1 +"
-    test "returns ok tuple when success on write" do
-      assert {:ok, @equation} = RPNCalculator.Output.write(MockResource, @filename, @equation)
-    end
-
-    @tag :pending
-    @filename "filename"
-    @equation "1 1 +"
-    test "closes the resource even when it errors" do
-      MockResource.set_test_failure(true)
-
-      assert {:error, "Unable to write to resource"} =
-               RPNCalculator.Output.write(MockResource, @filename, @equation)
-
-      with log <- MockResource.get_log(),
-           true <- length(log) == 1,
-           close_entry <- hd(log),
-           true <- String.starts_with?(close_entry, "[CLOSE]") do
-        assert true
-      else
-        _ ->
-          flunk("write/3 should close the `resource` even if an error is raised")
-      end
+    test "closes resource even when rescuing from raised error" do
+      RPNCalculator.Output.write(@resource, @bad_filename, @equation)
+      assert_received :close, "write/3 should close the `resource` even if an error is raised"
     end
   end
 end
