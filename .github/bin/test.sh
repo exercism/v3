6edf71541ac44fa7a5810580dbac25bc3a468fa8
Tim Austin <tim@neenjaw.com>
2020-06-19 13:27:37 -0600

    try -r option

--- .github/bin/test.sh
+++ .github/bin/test.sh
@@ -5,9 +5,6 @@ set -euo pipefail
 # PULL_REQUEST_URL=$(jq -r ".pull_request.url" "$GITHUB_EVENT_PATH")
 PULL_REQUEST_URL="https://api.github.com/repos/exercism/v3/pulls/1708"
 
-PR_FILES=$(curl --url "${PULL_REQUEST_URL}/files" --header "authorization: Bearer ${GH_TOKEN}" \
-             | jq -c '.[] | select(.status == "added" or .status == "modified") | select(.filename | match("\\.(md|json)$")) | .filename')
-
-
-   # | \
-  #xargs npx prettier@2.0.4 --check
+curl --url "${PULL_REQUEST_URL}/files" --header "authorization: Bearer ${GH_TOKEN}" |
+  jq -c '.[] | select(.status == "added" or .status == "modified") | select(.filename | match("\\.(md|json)$")) | .filename' |
+  xargs -r npx prettier@2.0.4 --check
