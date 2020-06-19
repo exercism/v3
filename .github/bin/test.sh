35607505318fe1c6cc372a4c639feb90bc8bbf9a
Tim Austin <tim@neenjaw.com>
2020-06-19 13:18:51 -0600

    pipe

--- .github/bin/test.sh
+++ .github/bin/test.sh
@@ -5,9 +5,8 @@ set -euo pipefail
 # PULL_REQUEST_URL=$(jq -r ".pull_request.url" "$GITHUB_EVENT_PATH")
 PULL_REQUEST_URL="https://api.github.com/repos/exercism/v3/pulls/1708"
 
-PR_FILES=$(curl --url "${PULL_REQUEST_URL}/files" --header "authorization: Bearer ${GH_TOKEN}")
-
-JQ_OUT=$(jq -c '.[] | select(.status == "added" or .status == "modified") | select(.filename | match("\\.(md|json)$")) | .filename' < "${PR_FILES}")
+PR_FILES=$(curl --url "${PULL_REQUEST_URL}/files" --header "authorization: Bearer ${GH_TOKEN}" \
+             | jq -c '.[] | select(.status == "added" or .status == "modified") | select(.filename | match("\\.(md|json)$")) | .filename')
 
 
    # | \
