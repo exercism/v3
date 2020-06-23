595dad623ea6c3acd1df9106ca30371d83d8c228
Erik Schierboom <erik_schierboom@hotmail.com>
2020-06-23 16:49:30 +0200

    Test

--- .github/bin/languages/update.sh
+++ .github/bin/languages/update.sh
@@ -6,17 +6,6 @@ dotnet run -p .github/bin/languages
 # Format the documents
 npx prettier@2.0.4 --write languages/README.md languages/languages.json
 
-# Fetch and prune remote
-git fetch --prune origin
-
-# Setup the git user (required to commit anything)
-git config --global user.email "github-actions[bot]@users.noreply.github.com"
-git config --global user.name "github-actions[bot]"
-
-# Checkout to new branch
-BRANCH="languagesummary-$(date +%Y%m%d%H%M%S)"
-git checkout -b "$BRANCH"
-
 # Add the updated language summary files
 git add languages/README.md
 git add languages/languages.json
@@ -27,8 +16,16 @@ if [ -z "$(git status --porcelain)" ]; then
     exit 0
 fi
 
+# Checkout to new branch
+BRANCH="languagesummary-$(date +%Y%m%d%H%M%S)"
+git checkout -b "$BRANCH"
+
 echo "Committing updated languages summary"
+
+# Setup the git user (required to commit anything)
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
 
 # Commit and push the changes
 git commit -m "[CI] Update languages summary"
-git push origin "HEAD:$BRANCH"
+git push origin "$BRANCH"
