965c7f8a6d4122ea1d708ebf973a35a15e29159e
Erik Schierboom <erik_schierboom@hotmail.com>
2020-06-23 12:26:41 +0200

    Add CI

--- .github/bin/exercises/update.sh
+++ .github/bin/exercises/update.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
 
-# Update the stories summary
-dotnet run -p .github/bin/stories
+# Update the exercises summary
+dotnet run -p .github/bin/exercises
 
 # Format the documents
-npx prettier@2.0.4 --write reference/stories/README.md reference/stories/stories.json
+npx prettier@2.0.4 --write languages/README.md languages/languages.json
 
-# Add the updated story summary files
-git add reference/stories/README.md
-git add reference/stories/stories.json
+# Add the updated language summary files
+git add languages/README.md
+git add languages/languages.json
 
-# Check if there is nothing to commit (i.e. no changes to the stories summary)
+# Check if there is nothing to commit (i.e. no changes to the exercises summary)
 if [ -z "$(git status --porcelain)" ]; then
-    echo "No changes to the stories summary"
+    echo "No changes to the exercises summary"
     exit 0
 fi
 
@@ -20,9 +20,9 @@ fi
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 
-echo "Committing updated stories summary"
+echo "Committing updated exercises summary"
 
 # Commit the changes
 git add .
-git commit -m "[CI] Update stories summary"
+git commit -m "[CI] Update exercises summary"
 git push
