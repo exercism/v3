77c395302b577096f02183ad658445322077a31a
mikedamay <mikemay.mdamay@googlemail.com>
2020-06-25 08:27:45 +0100

    csharp/randomness improved RollDie test

--- languages/csharp/exercises/concept/randomness/RandomnessTests.cs
+++ languages/csharp/exercises/concept/randomness/RandomnessTests.cs
@@ -7,8 +7,17 @@ public class RandomnessTests
     public void RollDie()
     {
         var player = new Player();
-        var pips = player.RollDie();
-        Assert.InRange(pips, 1, 18);
+        bool outOfRange = false;
+        for (int i = 0; i < 100; i++)
+        {
+            var pips = player.RollDie();
+            if (pips <= 0 || pips > 18)
+            {
+                outOfRange = true;
+                break;
+            }
+        }
+        Assert.False(outOfRange);
     }
 
     [Fact/*(Skip = "Remove this Skip property to run this test")*/]
