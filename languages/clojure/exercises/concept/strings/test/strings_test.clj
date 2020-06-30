a3be2a532cab8e54569b9fc5fe079b3d1963b55f
porkostomus <porkostomus@mail.com>
2020-06-30 15:27:41 -0700

    refactor tests

--- languages/clojure/exercises/concept/strings/test/strings_test.clj
+++ languages/clojure/exercises/concept/strings/test/strings_test.clj
@@ -2,35 +2,30 @@
   (:require [clojure.test :refer [deftest testing is]]
             strings))
 
-(deftest error-message-test
-  (is (= "Stack overflow" (strings/message "[ERROR]: Stack overflow"))))
+(deftest message-test
+  (testing "Error"
+    (is (= "Stack overflow" (strings/message "[ERROR]: Stack overflow")))
+    (testing "Warning"
+      (is (= (strings/message "[WARNING]: Disk almost full") "Disk almost full")))
+    (testing "Info"
+      (is (= (strings/message "[INFO]: File moved") "File moved"))
+      (testing "Trim whitespace"
+        (is (= "Timezone not set" (strings/message "[WARNING]:   \tTimezone not set  \r\n")))))))
 
-(deftest warning-message-test
-  (is (= (strings/message "[WARNING]: Disk almost full") "Disk almost full")))
+(deftest log-level-test
+  (testing "Error"
+    (is (= "error" (strings/log-level "[ERROR]: Disk full")))
+    (testing "Warning"
+      (is (= "warning" (strings/log-level "[WARNING]: Unsafe password"))))
+    (testing "Info"
+      (is (= "info" (strings/log-level "[INFO]: Timezone changed"))))))
 
-(deftest info-message-test
-  (is (= (strings/message "[INFO]: File moved") "File moved")))
-
-(deftest message-trim-test
-  (is (= "Timezone not set" (strings/message "[WARNING]:   \tTimezone not set  \r\n"))))
-
-(deftest error-log-level-test
-  (is (= "error" (strings/log-level "[ERROR]: Disk full"))))
-
-(deftest warning-log-level-test
-  (is (= "warning" (strings/log-level "[WARNING]: Unsafe password"))))
-
-(deftest info-log-level-test
-  (is (= "info" (strings/log-level "[INFO]: Timezone changed"))))
-
-(deftest error-reformat-test
-  (is (= "Segmentation fault (error)" (strings/reformat "[ERROR]: Segmentation fault"))))
-
-(deftest warning-reformat-test
-  (is (= "Decreased performance (warning)" (strings/reformat "[WARNING]: Decreased performance"))))
-
-(deftest info-reformat-test
-  (is (= "Disk defragmented (info)" (strings/reformat "[INFO]: Disk defragmented"))))
-
-(deftest reformat-trim-test
-  (is (= "Corrupt disk (error)" (strings/reformat "[ERROR]: \t Corrupt disk\t \t \r\n"))))
+(deftest reformat-test
+  (testing "Error"
+    (is (= "Segmentation fault (error)" (strings/reformat "[ERROR]: Segmentation fault")))
+    (testing "Warning"
+      (is (= "Decreased performance (warning)" (strings/reformat "[WARNING]: Decreased performance"))))
+    (testing "Info"
+      (is (= "Disk defragmented (info)" (strings/reformat "[INFO]: Disk defragmented"))))
+    (testing "Trim whitespace"
+      (is (= "Corrupt disk (error)" (strings/reformat "[ERROR]: \t Corrupt disk\t \t \r\n"))))))
