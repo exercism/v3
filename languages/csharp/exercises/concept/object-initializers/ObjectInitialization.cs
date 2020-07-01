1411f048f3e3c4559a6afde786454a8b5f15037f
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-01 08:07:56 -0400

    csharp/time ready for draft PR

--- languages/csharp/exercises/concept/object-initializers/ObjectInitialization.cs
+++ languages/csharp/exercises/concept/object-initializers/ObjectInitialization.cs
@@ -8,7 +8,6 @@ public class Authenticator
 
     // TODO: Impleent the Authenticator.Developers property
     public IDictionary<string, Identity> Developers { get; }
-
 }
 
 //**** please do not modify the FacialFeatures class ****
