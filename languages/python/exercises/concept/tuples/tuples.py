92265e45bd00344762d751b0876e56c5126633e4
BethanyG <bethanymgarcia@gmail.com>
2020-07-03 16:33:52 -0700

    New tuples concept exercise cleaned boilerplate file.

--- languages/python/exercises/concept/tuples/tuples.py
+++ languages/python/exercises/concept/tuples/tuples.py
@@ -1,28 +1,19 @@
 
 def get_coordinate(record):
-    return record[1]
+    pass
 
 def convert_coordinate(coordinate):
-    # alt     return tuple(coordinate)
-    return coordinate[0], coordinate[1]
+    pass
 
 
 def compare_records(azara_record, rui_record):
-    return  tuple(azara_record[1]) in rui_record
+    pass
 
 def create_record(azara_record, rui_record):
-    if compare_records(azara_record, rui_record):
-        return azara_record + rui_record
-    else:
-        return "not a match"
+    pass
 
 def clean_up(combined_record_group):
-    for item in combined_record_group:
-        new_item = item[0], item[2], item[3], item[4]
-        print(new_item)
+    pass
 
 def multiply_records(combined_record_group):
-    group_for_three = combined_record_group * 3
-
-    for item in group_for_three:
-        print(item)
+    pass
