8ff38f3259264f8026d71a9538f15b150d02441b
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-10 18:59:44 -0400

    Update bash script

--- languages/javascript/scripts/scaffold/scaffold.sh
+++ languages/javascript/scripts/scaffold/scaffold.sh
@@ -5,4 +5,13 @@ echo "Scaffolding '$1'"
 cp -r ./template ../../exercises/concept/$1
 
 echo "Template '$1' created for JS."
-echo "Replace all '---'s with the appropriate data."
+cd ../../exercises/concept/$1
+
+sed -i '' "s/---/$1/g" *
+
+mv concept-name.js $1.js
+mv concept-name.spec.js $1.js
+
+echo "Final directory:" && ls
+
+echo "Remember to fill out all details in every file!"
