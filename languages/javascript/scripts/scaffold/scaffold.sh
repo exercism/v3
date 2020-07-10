217bc7d0f67c73b9de78b323d5697fbae148da3e
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-10 18:18:34 -0400

    Add echo prompts

--- languages/javascript/scripts/scaffold/scaffold.sh
+++ languages/javascript/scripts/scaffold/scaffold.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Scaffolding '$1'"
+
 cp -r ./template ../../exercises/concept/$1
+
+echo "Template '$1' created for JS."
+echo "Replace all '---'s with the appropriate data."
