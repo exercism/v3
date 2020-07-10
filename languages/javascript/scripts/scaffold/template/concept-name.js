42538b7f25749a77fd738caf344004a4e1f368e4
Rishi Kothari <itsrishikothari@gmail.com>
2020-07-10 18:58:45 -0400

    Update documentation for all template JS files

---
+++ languages/javascript/scripts/scaffold/template/concept-name.js
@@ -0,0 +1,5 @@
+/*
+
+This is the stub implementation file, which is the starting point for students to work on the exercise.
+
+*/
--- languages/javascript/scripts/scaffold/template/example.js
+++ languages/javascript/scripts/scaffold/template/example.js
@@ -1,1 +1,3 @@
-/*---*/
+/*
+This is an example implementation that passes all the tests.
+*/
