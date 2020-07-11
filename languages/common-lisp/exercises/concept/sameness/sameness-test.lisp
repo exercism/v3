abe77b518f63af28f810b2c0358e507ebf83d1eb
Mark Simpson <verdammelt@gmail.com>
2020-07-11 08:57:25 -0400

    tests involving equal.
    Chose to skip pathnames as they have some implementation defined
    behavior around case-sensitivity. Should add a pathname exercise that
    can do something about that.

--- languages/common-lisp/exercises/concept/sameness/sameness-test.lisp
+++ languages/common-lisp/exercises/concept/sameness/sameness-test.lisp
@@ -17,14 +17,33 @@
 (def-suite sameness-suite)
 (in-suite sameness-suite)
 
+(defparameter +an-array+ #(1 2 3))
+(defparameter +a-similar-but-different-array+ #(1 2 3))
+
 (defparameter +mazes+
   '((:maze-1 . ((("wrong" "WRONG") . explosion)
                 ((2 2.0) . explosion)
                 ((lisp LISP) . victory)))
+
     (:maze-2 . (((#\a #\A) . explosion)
                 ((#\a #\a) . victory)))
     (:maze-3 . (((1.0 1) . explosion)
-                ((1.0 1.0) . victory))))
+                ((1.0 1.0) . victory)))
+
+    (:maze-4 . ((((a . b) (a . c)) . explosion)
+                (((a . b) (a . b)) . victory)))
+    (:maze-5 . ((((#\a . #\b) (#\A . #\b)) . explosion)
+                (((#\a . #\b) (#\a . #\b)) . victory)))
+    (:maze-6 . ((((1 . 2) (1 . 2.0)) . explosion)
+                (((1 . 2) (1 . 2)) . victory)))
+
+    (:maze-7 . (((+an-array+ +a-similar-but-different-array+) . explosion)
+                ((+an-array+ +an-array+) . victory)))
+
+    (:maze-8 . ((("wrong" "WRONG") . explosion)
+                (("lisp" "lisp") . victory)))
+
+    )
   "Mazes are a sequence of pairs of a DOOR and a RESULT. A DOOR is a sequence of
 things which will be given to the key. If a KEY opens a DOOR, the maze will
 evaluate to RESULT")
@@ -35,6 +54,15 @@ evaluate to RESULT")
   (is (eq 'victory (run-maze :maze-2 #'robot))))
 (test the-maze-of-numbers
   (is (eq 'victory (run-maze :maze-3 #'robot))))
+(test the-maze-of-conses
+  (is (eq 'victory (run-maze :maze-4 #'robot)))
+  (is (eq 'victory (run-maze :maze-5 #'robot)))
+  (is (eq 'victory (run-maze :maze-6 #'robot))))
+
+(test the-maze-of-arrays
+  (is (eq 'victory (run-maze :maze-7 #'robot)))
+  (is (eq 'victory (run-maze :maze-8 #'robot))))
+
 ;; tests that must use equal (lists, arrays (eq), strings)
 
 ;; tests that must use equalp (characters (case insensitive) arrays (numbers for
