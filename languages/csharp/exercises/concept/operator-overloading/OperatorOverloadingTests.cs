c13893329628ee97587766a18b3ea74d97e0a5fe
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-11 03:45:38 -0400

    csharp/operator-overloading typo

--- languages/csharp/exercises/concept/operator-overloading/OperatorOverloadingTests.cs
+++ languages/csharp/exercises/concept/operator-overloading/OperatorOverloadingTests.cs
@@ -10,13 +10,13 @@ public class OperatorOverloadingTests
     }
 
     [Fact /*(Skip = "Remove this Skip property to run this test")*/]
-    public void EQuality_false()
+    public void Equality_false()
     {
         Assert.False(Eq(new CurrencyAmount(55, "HD"), new CurrencyAmount(60, "HD")));
     }
 
     [Fact /*(Skip = "Remove this Skip property to run this test")*/]
-    public void EQuality_bad()
+    public void Equality_bad()
     {
         Assert.Throws<ArgumentException>(() => Eq(new CurrencyAmount(55, "HD"), new CurrencyAmount(60, "USD")));
     }
