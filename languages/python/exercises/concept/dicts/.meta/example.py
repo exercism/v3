f9866e5d18458af2800d7b23741d281b65b5604a
Job van der Wal <48634934+J08K@users.noreply.github.com>
2020-07-13 16:32:06 +0200

    Update languages/python/exercises/concept/dicts/.meta/example.py
    Co-authored-by: Corey McCandless <cmccandless@users.noreply.github.com>

--- languages/python/exercises/concept/dicts/.meta/example.py
+++ languages/python/exercises/concept/dicts/.meta/example.py
@@ -15,7 +15,7 @@ def delete_items(inventory, items):
     for item in items:
         inventory.setdefault(item, 0)
         inventory[item] = max(inventory[item] - 1, 0)
-    return(inventory)
+    return inventory
 
 def list_inventory(inventory):
     return [(k, v) for k, v in sorted(inventory.items()) if v]
