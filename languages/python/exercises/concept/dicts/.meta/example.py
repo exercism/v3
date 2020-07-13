11b6b10046f69f6cbc78db9f68d3de236db203ce
Job van der Wal <48634934+J08K@users.noreply.github.com>
2020-07-13 23:33:58 +0200

    Update example.py

--- languages/python/exercises/concept/dicts/.meta/example.py
+++ languages/python/exercises/concept/dicts/.meta/example.py
@@ -1,7 +1,8 @@
 def create_inventory(items):
     inventory = dict()
-	add_items(inventory, items)
+    add_items(inventory, items)
     return inventory
+
 
 def add_items(inventory, items):
     for item in items:
@@ -9,11 +10,13 @@ def add_items(inventory, items):
         inventory[item] += 1
     return inventory
 
+
 def delete_items(inventory, items):
     for item in items:
         inventory.setdefault(item, 0)
         inventory[item] = max(inventory[item] - 1, 0)
     return inventory
+
 
 def list_inventory(inventory):
     return [(k, v) for k, v in sorted(inventory.items()) if v]
