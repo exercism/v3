afb00c6192a0365317681ea5b93b0b8a71c22cd0
Benjamin Großmann <benjamin.grossmann@gfk.com>
2020-07-14 21:24:15 +0200

    skip all tests except the first one per default to allow students to enable them one by one

--- languages/scala/exercises/concept/basics/LasagnaTest.scala
+++ languages/scala/exercises/concept/basics/LasagnaTest.scala
@@ -6,22 +6,27 @@ class LasagnaTest extends FunSuite with Matchers {
   }
 
   test("remaining minutes in oven after 25 minutes") {
+    pending
     new Lasagna().remainingMinutesInOven(25) should be(15)
   }
 
   test("preparation time in minutes for one layer") {
+    pending
     new Lasagna().preparationTimeInMinutes(1) should be(2)
   }
 
   test("preparation time in minutes for multiple layers") {
+    pending
     new Lasagna().preparationTimeInMinutes(4) should be(8)
   }
 
   test("elapsed time in minutes for one layer") {
+    pending
     new Lasagna().elapsedTimeInMinutes(1, 30) should be(32)
   }
 
   test("elapsed time in minutes for multiple layers") {
+    pending
     new Lasagna().elapsedTimeInMinutes(4, 8) should be(16)
   }
 }
