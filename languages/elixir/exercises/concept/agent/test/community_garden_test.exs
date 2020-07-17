a3f8b26c37418d63222ee054210f60ea22ebae01
Tim Austin <tim@neenjaw.com>
2020-07-17 10:46:55 -0600

    add pending tags

--- languages/elixir/exercises/concept/agent/test/community_garden_test.exs
+++ languages/elixir/exercises/concept/agent/test/community_garden_test.exs
@@ -1,27 +1,32 @@
 defmodule CommunityGardenTest do
   use ExUnit.Case
 
-  test "start returns a alive pid" do
+  # @tag :pending
+  test "start returns an alive pid" do
     assert {:ok, pid} = CommunityGarden.start()
     assert Process.alive?(pid)
   end
 
+  @tag :pending
   test "when started, the registry is empty" do
     assert {:ok, pid} = CommunityGarden.start()
     assert [] == CommunityGarden.list_registrations(pid)
   end
 
+  @tag :pending
   test "can register a new plot" do
     assert {:ok, pid} = CommunityGarden.start()
     assert %Plot{} = CommunityGarden.register(pid, "Johnny Appleseed")
   end
 
+  @tag :pending
   test "maintains a registry of plots" do
     assert {:ok, pid} = CommunityGarden.start()
     assert %Plot{} = plot = CommunityGarden.register(pid, "Johnny Appleseed")
     assert [plot] == CommunityGarden.list_registrations(pid)
   end
 
+  @tag :pending
   test "can release a plot" do
     assert {:ok, pid} = CommunityGarden.start()
     assert %Plot{} = plot = CommunityGarden.register(pid, "Johnny Appleseed")
@@ -29,12 +34,14 @@ defmodule CommunityGardenTest do
     assert [] == CommunityGarden.list_registrations(pid)
   end
 
+  @tag :pending
   test "can get a specific registration by id" do
     assert {:ok, pid} = CommunityGarden.start()
     assert %Plot{} = plot = CommunityGarden.register(pid, "Johnny Appleseed")
     assert "Johnny Appleseed" = CommunityGarden.get_registration(pid, plot.plot_id).registered_to
   end
 
+  @tag :pending
   test "can get registration of a registered plot" do
     assert {:ok, pid} = CommunityGarden.start()
     assert %Plot{} = plot = CommunityGarden.register(pid, "Johnny Appleseed")
@@ -43,6 +50,7 @@ defmodule CommunityGardenTest do
     assert registered_plot.registered_to == "Johnny Appleseed"
   end
 
+  @tag :pending
   test "return not_found when attempt to get registration of an unregistered plot" do
     assert {:ok, pid} = CommunityGarden.start()
     assert {:not_found, "plot is unregistered"} = CommunityGarden.get_registration(pid, 1)
