9409c45e7d129efadc5b72817b11eeca3fca9b1e
Samuel Teixeira <samuelteixeiras@gmail.com>
2020-07-22 21:06:11 +0100

    Adding Array Concept

---
+++ languages/java/exercises/concept/arrays/src/main/java/BirdCount.java
@@ -0,0 +1,39 @@
+
+class BirdCount {
+    private int[] birdsPerDay;
+
+    public BirdCount(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay;
+    }
+
+
+    public int[] getLastWeek() {
+        // TODO Please implement the BirdCount.getLastWeek() method
+        return null;
+    }
+
+    public int getToday() {
+        // TODO Please implement the BirdCount.getToday() method
+        return 0;
+    }
+
+    public int incrementTodaysCount() {
+        // TODO Please implement the BirdCount.incrementTodaysCount() method"
+        return 0;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        // TODO Please implement the BirdCount.hasDayWithoutBirds() method
+        return false;
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        // TODO Please implement the BirdCount.getCountForFirstDays() method
+        return 0;
+    }
+
+    public int getBusyDays() {
+        // TODO Please implement the BirdCount.getBusyDays() method
+        return 0;
+    }
+}
---
+++ languages/java/exercises/concept/arrays/src/test/java/BirdCountTest.java
@@ -0,0 +1,83 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class BirdCountTest {
+
+
+    private BirdCount birdCount;
+
+    private int lastWeek[] = {0, 2, 5, 3, 7, 8, 4};
+
+
+    @Before
+    public void setUp() {
+        birdCount = new BirdCount(lastWeek);
+    }
+
+
+    @Test
+    public void itTestGetLastWeek() {
+        assertEquals(true, birdCount.getLastWeek().equals(lastWeek));
+    }
+
+    @Test
+    public void itTestGetToday() {
+        assertEquals(lastWeek[lastWeek.length - 1], birdCount.getToday());
+    }
+
+    @Test
+    public void itShouldReturnZeroIfBirdCountLastWeekIsEmpty() {
+        int[] lastWeekEmpty = new int[0];
+        birdCount = new BirdCount(lastWeekEmpty);
+        assertEquals(0, birdCount.getToday());
+    }
+
+    @Test
+    public void itIncrementTodaysCount() {
+        int currentTodayCount = birdCount.getToday();
+        birdCount.incrementTodaysCount();
+        assertEquals(currentTodayCount + 1, birdCount.getToday());
+    }
+
+    @Test
+    public void itHasDayWithoutBirds() {
+        assertEquals(true, birdCount.hasDayWithoutBirds());
+    }
+
+    @Test
+    @Ignore
+    public void itShouldNotHaveDaysWithoutBirds() {
+        birdCount = new BirdCount(new int[]{1, 2, 5, 3, 7, 8, 4});
+        assertEquals(false, birdCount.hasDayWithoutBirds());
+    }
+
+
+    @Test
+    public void itTestGetCountForFirstDays() {
+        assertEquals(10, birdCount.getCountForFirstDays(4));
+    }
+
+    @Test
+    @Ignore
+    public void itTestGetCountForMoreDaysThanTheArraySize() {
+        assertEquals(29, birdCount.getCountForFirstDays(10));
+    }
+
+    @Test
+    public void itTestGetCountForBusyDays() {
+        assertEquals(3, birdCount.getBusyDays());
+    }
+
+    @Test
+    @Ignore
+    public void itShouldNotHaveBusyDays() {
+        birdCount = new BirdCount(new int[]{1, 2, 3, 3, 2, 1, 4});
+        assertEquals(0, birdCount.getBusyDays());
+    }
+
+
+}
