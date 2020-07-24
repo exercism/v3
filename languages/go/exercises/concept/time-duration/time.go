e6f8732b612a1acb9c4f360fe33842a668960f21
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-24 23:43:18 +0530

    Update time.go

--- languages/go/exercises/concept/time-duration/time.go
+++ languages/go/exercises/concept/time-duration/time.go
@@ -3,26 +3,26 @@ package time
 import "time"
 
 // StartTime returns the time duration in string format
-func StartTime(date string) time.Time {
+func StartTime(s string) time.Duration {
 	panic("Please implement the StartTime function")
 }
 
 // IsOver returns whether a class is over or not
-func IsOver(date string) bool {
+func IsOver(s1 string,s2 string) bool {
 	panic("Please implement the IsOver function")
 }
 
 // ExtraTime returns extra time elapsed by the class in minutes
-func ExtraTime(date string) bool {
+func ExtraTime(s1 string,s2 string) float64 {
 	panic("Please implement the ExtraTime function")
 }
 
 // Display returns the total time passed in string format
-func Display(date string) string {
+func Display(s1 string,s2 string) string {
 	panic("Please implement the Display function")
 }
 
 // RoundOff returns the rounded off time in some given minutes
-func RoundOff() time.Time {
+func RoundOff(s1 string,s2 string) time.Duration {
 	panic("Please implement the RoundOff function")
 }
