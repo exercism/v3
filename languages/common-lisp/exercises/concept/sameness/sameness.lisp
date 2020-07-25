933af0b35fe867e9b402b2bfd786761d81f20229
Mark Simpson <verdammelt@gmail.com>
2020-07-25 15:59:44 -0400

    rework names of mazes, rooms and keys.
    In order to make them more similar to each other.
    
    Fixes #2065

--- languages/common-lisp/exercises/concept/sameness/sameness.lisp
+++ languages/common-lisp/exercises/concept/sameness/sameness.lisp
@@ -1,34 +1,33 @@
 (defpackage sameness
   (:use :cl)
   (:export
-   :key-object-indentity
+   :key-object-identity
    :key-numbers
-   :key-looser-numbers
-   :key-chars
-   :key-insensitive-chars
+   :key-numbers-of-different-types
+   :key-characters
+   :key-characters-case-insensitively
    :key-strings
-   :key-insensitive-string
-   :key-cons-symbols
-   :key-cons-chars
-   :key-cons-numbers
-   :key-cons-insensitive-chars
-   :key-cons-looser-numbers
+   :key-strings-case-insensitively
+   :key-conses-of-symbols
+   :key-conses-of-characters
+   :key-conses-of-numbers
+   :key-conses-of-characters-case-insensitively
+   :key-conses-of-numbers-of-different-types
    :key-arrays
-   :key-arrays-looser-equal))
-
+   :key-arrays-loosely))
 (in-package :sameness)
 
-(defun key-object-indentity (x y))
+(defun key-object-identity (x y))
 (defun key-numbers (x y))
-(defun key-looser-numbers (x y))
-(defun key-chars (x y))
-(defun key-insensitive-chars (x y))
+(defun key-numbers-of-different-types (x y))
+(defun key-characters (x y))
+(defun key-characters-case-insensitively (x y))
 (defun key-strings (x y))
-(defun key-insensitive-string (x y))
-(defun key-cons-symbols (x y))
-(defun key-cons-chars (x y))
-(defun key-cons-numbers (x y))
-(defun key-cons-insensitive-chars (x y))
-(defun key-cons-looser-numbers (x y))
+(defun key-strings-case-insensitively (x y))
+(defun key-conses-of-symbols (x y))
+(defun key-conses-of-characters (x y))
+(defun key-conses-of-numbers (x y))
+(defun key-conses-of-characters-case-insensitively (x y))
+(defun key-conses-of-numbers-of-different-types (x y))
 (defun key-arrays (x y))
-(defun key-arrays-looser-equal (x y))
+(defun key-arrays-loosely (x y))
