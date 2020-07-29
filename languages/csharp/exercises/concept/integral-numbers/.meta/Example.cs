53e9914f5b9f97a6f93821755178ca7f4aa42df6
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-29 07:24:35 -0400

    csharp/integral-numbers - proofing

--- languages/csharp/exercises/concept/integral-numbers/.meta/Example.cs
+++ languages/csharp/exercises/concept/integral-numbers/.meta/Example.cs
@@ -9,37 +9,37 @@ public static class TelemetryBuffer
         byte[] bytes;
         if (reading > UInt32.MaxValue || reading < Int32.MinValue)
         {
-            allBytes[0] = unchecked((byte)-8);
+            allBytes[0] = 0xf8;
             bytes = BitConverter.GetBytes(reading);
             bytes.CopyTo(allBytes, 1);
         }
         else if (reading > Int32.MaxValue)
         {
-            allBytes[0] = 4;
+            allBytes[0] = 0x4;
             bytes = BitConverter.GetBytes((uint)reading);
             bytes.CopyTo(allBytes, 1);
         }
         else if (reading > UInt16.MaxValue || reading < Int16.MinValue)
         {
-            allBytes[0] = unchecked((byte)-4);
+            allBytes[0] = 0xfc;
             bytes = BitConverter.GetBytes((int) reading);
             bytes.CopyTo(allBytes, 1);
         }
         else if (reading > Int16.MaxValue)
         {
-            allBytes[0] = 2;
+            allBytes[0] = 0x2;
             bytes = BitConverter.GetBytes((ushort) reading);
             bytes.CopyTo(allBytes, 1);
         }
         else if (reading >= 0)
         {
-            allBytes[0] = unchecked((byte)-2);
+            allBytes[0] = 0xfe;
             bytes = BitConverter.GetBytes((ushort) reading);
             bytes.CopyTo(allBytes, 1);
         }
         else
         {
-            allBytes[0] = unchecked((byte)-2);
+            allBytes[0] = 0xfe;
             bytes = BitConverter.GetBytes((short) reading);
             bytes.CopyTo(allBytes, 1);
         }
