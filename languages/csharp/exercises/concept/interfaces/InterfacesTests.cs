9d762ebd3ad20a70e51db0d75481ace8308089ee
mikedamay <mikemay.mdamay@googlemail.com>
2020-07-30 01:35:02 -0400

    csharp/interfaces added test for IComparable

--- languages/csharp/exercises/concept/interfaces/InterfacesTests.cs
+++ languages/csharp/exercises/concept/interfaces/InterfacesTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 public class InterfacesTests
@@ -14,7 +15,7 @@ public class InterfacesTests
         Assert.Equal(20, experimentalCar.DistanceTravelled - productionCar.DistanceTravelled);
     }
 
-    [Fact/*(Skip = "Remove this Skip property to run this test")*/]
+    [Fact(Skip = "Remove this Skip property to run this test")]
     public void RankCars()
     {
         var prc1 = new ProductionRemoteControlCar();
@@ -23,5 +24,19 @@ public class InterfacesTests
         prc2.NumberOfVictories = 2;
         var rankings = TestTrack.GetRankedCars(prc1, prc2);
         Assert.Same(prc1, rankings[1]);
+    }
+
+    [Fact(Skip = "Remove this Skip property to run this test")]
+    public void EnsureCarsAreComparable()
+    {
+        var fast = new ProductionRemoteControlCar();
+        var medium = new ProductionRemoteControlCar();
+        var slow = new ProductionRemoteControlCar();
+        fast.NumberOfVictories = 3;
+        medium.NumberOfVictories = 2;
+        slow.NumberOfVictories = 1;
+        var cars = new List<ProductionRemoteControlCar> {fast, slow, medium};
+        cars.Sort();
+        Assert.Equal(new ProductionRemoteControlCar[] {slow, medium, fast}, cars);
     }
 }
