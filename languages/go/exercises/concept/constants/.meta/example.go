53e8c69b7148d4cf8ca830ca22d8b2352d011b00
jamessouth <jamesdanielsouth@gmail.com>
2020-08-02 21:29:21 -0500

    await feedback on slack

--- languages/go/exercises/concept/constants/.meta/example.go
+++ languages/go/exercises/concept/constants/.meta/example.go
@@ -1,4 +1,16 @@
 package _meta
 
-// FixedInterestRate is an untyped numeric constant holding the value 5
-const FixedInterestRate = 5
+/*
+Which is best if we want an unchanging value with the most type flexibility?
+
+a) const FixedInterestRate = 0.05
+b) var FixedInterestRate = 0.05
+c) const FixedInterestRate float32 = 0.05
+*/
+
+/**/
+
+func Answer1() string {
+	// return a, b, or c
+	return "a"
+}
