c1240aa39a522ba53f30a89062c15b6441c65b86
Martin Carlsson <martin.carlsson@svt.se>
2020-08-10 17:40:50 +0200

    Fixes style for @Ignore.

--- languages/java/exercises/concept/chars/src/test/java/IdentifierTest.java
+++ languages/java/exercises/concept/chars/src/test/java/IdentifierTest.java
@@ -10,49 +10,49 @@ public class IdentifierTest {
         assertThat(Identifier.clean("")).isEmpty();
     }
 
-    @Ignore("")
+    @Ignore("Remove to run test")
     @Test
     public void single_letter() {
         assertThat(Identifier.clean("A")).isEqualTo("A");
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void string() {
         assertThat(Identifier.clean("àḃç")).isEqualTo("àḃç");
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void spaces() {
         assertThat(Identifier.clean("my   Id")).isEqualTo("my___Id");
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void ctrl() {
         assertThat(Identifier.clean("my\0Id")).isEqualTo("myCTRLId");
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void string_with_no_letters() {
         assertThat(Identifier.clean("\uD83D\uDE00\uD83D\uDE00\uD83D\uDE00")).isEmpty();
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void kebab_to_camel_case() {
         assertThat(Identifier.clean("à-ḃç")).isEqualTo("àḂç");
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void omit_lower_case_greek_letters() {
         assertThat(Identifier.clean("MyΟβιεγτFinder")).isEqualTo("MyΟFinder");
     }
 
-    @Ignore
+    @Ignore("Remove to run test")
     @Test
     public void combine_conversions() {
         assertThat(Identifier.clean("9 -abcĐ\uD83D\uDE00ω\0")).isEqualTo("_AbcĐCTRL");
