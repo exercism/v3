91a6afd3f64885b38e54a5679647144cd01a9fa0
Tim Austin <tim@neenjaw.com>
2020-08-23 22:37:54 -0600

    all but first test skipped

--- languages/javascript/exercises/concept/closures/closures.spec.js
+++ languages/javascript/exercises/concept/closures/closures.spec.js
@@ -30,20 +30,20 @@ describe('closures', () => {
     const x1 = 0
     const y1 = 0
     const expected = [3, -5]
-    test(`should be predictable`, () => {
+    xtest(`should be predictable`, () => {
       expect(translator(x1, y1)).toEqual(expected)
     })
 
     const x2 = 4
     const y2 = 5
     const reusedExpected = [7, 0]
-    test('should be reusable', () => {
+    xtest('should be reusable', () => {
       expect(translator(x2, y2)).toEqual(reusedExpected)
     })
   })
 
   describe('scale2d', () => {
-    test('should return a function', () => {
+    xtest('should return a function', () => {
       expect(typeof scale2d(0, 0)).toBe('function')
     })
 
@@ -53,14 +53,14 @@ describe('closures', () => {
     const x1 = 1
     const y1 = 1
     const expected = [4, 2]
-    test(`should be predictable`, () => {
+    xtest(`should be predictable`, () => {
       expect(scaler(x1, y1)).toEqual(expected)
     })
 
     const x2 = -2
     const y2 = 5
     const reusedExpected = [-8, 10]
-    test('should be reusable', () => {
+    xtest('should be reusable', () => {
       expect(scaler(x2, y2)).toEqual(reusedExpected)
     })
   })
@@ -73,43 +73,43 @@ describe('closures', () => {
     const sy = 2
     const scaler = scale2d(sx, sy)
 
-    test('should return a function', () => {
+    xtest('should return a function', () => {
       expect(typeof composeTransform(translator, scaler)).toBe('function')
     })
 
-    test('should compose two translate functions', () => {
+    xtest('should compose two translate functions', () => {
       const composeTranslate = composeTransform(translator, translator)
       expect(composeTranslate(0, 0)).toEqual([-12, 20])
     })
 
-    test('should compose two scale functions', () => {
+    xtest('should compose two scale functions', () => {
       const composeScale = composeTransform(scaler, scaler)
       expect(composeScale(1, 1)).toEqual([9, 4])
     })
 
-    test('should compose in the correct order: g(f(x))', () => {
+    xtest('should compose in the correct order: g(f(x))', () => {
       const composed = composeTransform(scaler, translator)
       expect(composed(0, 0)).toEqual([-6, 10])
     })
 
-    test('should compose in the opposite order: g(f(x))', () => {
+    xtest('should compose in the opposite order: g(f(x))', () => {
       const composed = composeTransform(translator, scaler)
       expect(composed(0, 0)).toEqual([-18, 20])
     })
   })
 
   describe('memoizeTransform', () => {
-    test('should return a function', () => {
+    xtest('should return a function', () => {
       expect(typeof memoizeTransform(translate2d(0, 0))).toBe('function')
     })
 
-    test('should return the same result if given the same input', () => {
+    xtest('should return the same result if given the same input', () => {
       const memoizedTranslate = memoizeTransform(translate2d(2, 2))
       expect(memoizedTranslate(2, 2)).toEqual([4, 4])
       expect(memoizedTranslate(2, 2)).toEqual([4, 4])
     })
 
-    test('should not call the memoized function if the input is the same', () => {
+    xtest('should not call the memoized function if the input is the same', () => {
       const memoizedTransform = memoizeTransform(fakeTransform())
       expect(memoizedTransform(5, 5)).toEqual([1, 1])
       expect(memoizedTransform(5, 5)).toEqual([1, 1])
