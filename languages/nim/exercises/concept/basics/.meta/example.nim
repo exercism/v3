7359461d2f542a22a88b49536c03cc3efb8b85d2
Yoni Fihrer <yoni@fihrer.com>
2020-09-04 01:46:21 +0300

    Revert "Modify `basics` `example.nim` to have a compilation error"
    This reverts commit 40aabb3624ea0d5080f883421e4b9ba1e167fe4b.

--- languages/nim/exercises/concept/basics/.meta/example.nim
+++ languages/nim/exercises/concept/basics/.meta/example.nim
@@ -3,16 +3,16 @@ let expectedMinutesInOven* = 40
 let preparationMinutesPerLayer = 2 ## \
   ## The amount of minutes it takes to prepare a single layer.
 
-proc remainingMinutesInOven*(actualMinutesInOven: int) =
+proc remainingMinutesInOven*(actualMinutesInOven: int): int =
   ## Determine the amount of minutes the lasagna still needs to remain in the
   ## oven to be properly prepared.
   expectedMinutesInOven - actualMinutesInOven
 
-proc preparationTimeInMinutes*(numberOfLayers: int) =
+proc preparationTimeInMinutes*(numberOfLayers: int): int =
   ## Given a number of layers, determine the total preparation time.
   numberOfLayers * preparationMinutesPerLayer
 
-proc totalTimeInMinutes*(numberOfLayers, actualMinutesInOven: int) =
+proc totalTimeInMinutes*(numberOfLayers, actualMinutesInOven: int): int =
   ## Calculate the total working time. That is, the time to prepare all the layers
   ## of lasagna, and the time already spent in the oven.
   preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven
