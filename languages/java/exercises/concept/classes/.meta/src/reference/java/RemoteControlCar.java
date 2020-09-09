147057ef00ccc096200cc34a651c3150e9ea873e
mikedamay <mikemay.mdamay@googlemail.com>
2020-09-09 13:25:25 -0400

    java/classes - address missed review point

--- languages/java/exercises/concept/classes/.meta/src/reference/java/RemoteControlCar.java
+++ languages/java/exercises/concept/classes/.meta/src/reference/java/RemoteControlCar.java
@@ -1,34 +1,27 @@
-class RemoteControlCar
-{
+class RemoteControlCar {
     private int batteryPercentage = 100;
     private int distanceDrivenInMeters = 0;
 
-    public void drive()
-    {
-        if (batteryPercentage > 0)
-        {
+    public void drive() {
+        if (batteryPercentage > 0) {
             batteryPercentage--;
             distanceDrivenInMeters += 20;
         }
     }
 
-    public String distanceDisplay()
-    {
+    public String distanceDisplay() {
         return "Driven " + distanceDrivenInMeters + " meters";
     }
 
-    public String batteryDisplay()
-    {
-        if (batteryPercentage == 0)
-        {
+    public String batteryDisplay() {
+        if (batteryPercentage == 0) {
             return "Battery empty";
         }
 
         return "Battery at " + batteryPercentage + "%";
     }
 
-    public static RemoteControlCar buy()
-    {
+    public static RemoteControlCar buy() {
         return new RemoteControlCar();
     }
 }
--- languages/java/exercises/concept/classes/src/test/java/RemoteControlCarTest.java
+++ languages/java/exercises/concept/classes/src/test/java/RemoteControlCarTest.java
@@ -2,19 +2,16 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class RemoteControlCarTest
-{
+public class RemoteControlCarTest {
     @Test
-    public void Buy_new_car_returns_instance()
-    {
+    public void Buy_new_car_returns_instance() {
         RemoteControlCar car = RemoteControlCar.buy();
         Assert.assertNotNull(car);
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void Buy_new_car_returns_new_car_each_time()
-    {
+    public void Buy_new_car_returns_new_car_each_time() {
         RemoteControlCar car1 = RemoteControlCar.buy();
         RemoteControlCar car2 = RemoteControlCar.buy();
         Assert.assertNotSame(car2, car1);
@@ -22,24 +19,21 @@ public class RemoteControlCarTest
 
     @Ignore("Remove to run test")
     @Test
-    public void New_car_distance_display()
-    {
+    public void New_car_distance_display() {
         RemoteControlCar car = new RemoteControlCar();
         Assert.assertEquals("Driven 0 meters", car.distanceDisplay());
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void New_car_battery_display()
-    {
+    public void New_car_battery_display() {
         RemoteControlCar car = new RemoteControlCar();
         Assert.assertEquals("Battery at 100%", car.batteryDisplay());
     }
 
     @Ignore("Remove to run test")
     @Test
-    public void Distance_display_after_driving_once()
-    {
+    public void Distance_display_after_driving_once() {
         RemoteControlCar car = new RemoteControlCar();
         car.drive();
         Assert.assertEquals("Driven 20 meters", car.distanceDisplay());
@@ -47,12 +41,10 @@ public class RemoteControlCarTest
 
     @Ignore("Remove to run test")
     @Test
-    public void Distance_display_after_driving_multiple_times()
-    {
+    public void Distance_display_after_driving_multiple_times() {
         RemoteControlCar car = new RemoteControlCar();
 
-        for (int i = 0; i < 17; i++)
-        {
+        for (int i = 0; i < 17; i++) {
             car.drive();
         }
 
@@ -61,8 +53,7 @@ public class RemoteControlCarTest
 
     @Ignore("Remove to run test")
     @Test
-    public void Battery_display_after_driving_once()
-    {
+    public void Battery_display_after_driving_once() {
         RemoteControlCar car = new RemoteControlCar();
         car.drive();
         Assert.assertEquals("Battery at 99%", car.batteryDisplay());
@@ -70,12 +61,10 @@ public class RemoteControlCarTest
 
     @Ignore("Remove to run test")
     @Test
-    public void Battery_display_after_driving_multiple_times()
-    {
+    public void Battery_display_after_driving_multiple_times() {
         RemoteControlCar car = new RemoteControlCar();
 
-        for (int i = 0; i < 23; i++)
-        {
+        for (int i = 0; i < 23; i++) {
             car.drive();
         }
 
@@ -84,13 +73,11 @@ public class RemoteControlCarTest
 
     @Ignore("Remove to run test")
     @Test
-    public void Battery_display_when_battery_empty()
-    {
+    public void Battery_display_when_battery_empty() {
         RemoteControlCar car = new RemoteControlCar();
 
         // Drain the battery
-        for (int i = 0; i < 100; i++)
-        {
+        for (int i = 0; i < 100; i++) {
             car.drive();
         }
 
@@ -102,13 +89,11 @@ public class RemoteControlCarTest
 
     @Ignore("Remove to run test")
     @Test
-    public void Distance_display_when_battery_empty()
-    {
+    public void Distance_display_when_battery_empty() {
         RemoteControlCar car = new RemoteControlCar();
 
         // Drain the battery
-        for (int i = 0; i < 100; i++)
-        {
+        for (int i = 0; i < 100; i++) {
             car.drive();
         }
 
