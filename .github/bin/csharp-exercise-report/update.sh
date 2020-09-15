c386e4ef5da4527839042ac02722bc59b897d789
mikedamay <mikemay.mdamay@googlemail.com>
2020-09-15 04:37:40 -0400

    test-only

--- .github/bin/csharp-exercise-report/update.sh
+++ .github/bin/csharp-exercise-report/update.sh
@@ -34,4 +34,4 @@ git commit -m "[Bot] Update csharp/exercise-report document"
 git push origin "$BRANCH"
 
 # Create a PR
-gh pr create --title "[Bot] Update csharp/exercises document" --body "This is an _automatically generated_ PR to update the csharp exercises files." --label "type/bot" --base "master"
+gh pr create --title  "[Bot] Update csharp/exercises document" --body "This is an _automatically generated_ PR to update the csharp exercises files." --label "type/bot" --base "exercise-report-workflow"
