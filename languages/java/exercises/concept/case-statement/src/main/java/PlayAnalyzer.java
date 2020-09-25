c234670be6a7a7b4977daf8a202a7e65df7a4c61
Emilyen Laffineur <43468801+Azumix@users.noreply.github.com>
2020-09-26 00:03:55 +0200

    Update languages/java/exercises/concept/case-statement/src/main/java/PlayAnalyzer.java
    Co-authored-by: Erik Schierboom <erik_schierboom@hotmail.com>

--- languages/java/exercises/concept/case-statement/src/main/java/PlayAnalyzer.java
+++ languages/java/exercises/concept/case-statement/src/main/java/PlayAnalyzer.java
@@ -1,6 +1,5 @@
-public class PlayAnalyzer{
-    
-    public static String anaylizeOnField(int shirtNum){
-        //Implement here the exercice.
+public class PlayAnalyzer {    
+    public static String onField(int shirtNum) {
+        throw new UnsupportedOperationException("Please implement the (static) PlayAnalyzer.onField() method");
     }
 }
