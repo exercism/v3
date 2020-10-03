22b6cadc05a03ca5c46921c7ba83e647f996446c
Emilyen Laffineur <emilyen.laffineur@lilo.org>
2020-10-03 12:47:00 +0200

    [Java] Add exercise case-statement update tests
    Update tests :
    	- Add @Ignore("remove to run test") on all but first
    	- Change the order to reflect the exercise progression

--- languages/java/exercises/concept/case-statement/src/test/java/PlayAnalyzerTest.java
+++ languages/java/exercises/concept/case-statement/src/test/java/PlayAnalyzerTest.java
@@ -3,38 +3,32 @@ import org.junit.Test;
 
 public class PlayAnalyzerTest {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void test_exception(){
-        PlayAnalyzer.onField(13);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void test_exception_negative_number(){
-        PlayAnalyzer.onField(-1);
-    }
-
     @Test
     public void test_goal(){
         Assert.assertEquals("goalie", PlayAnalyzer.onField(1));
     }
 
     @Test
+    @Ignore("Remove to run test")
     public void test_left_back(){
         Assert.assertEquals("left back", PlayAnalyzer.onField(2));
     }
 
     @Test
+    @Ignore("Remove to run test")
     public void test_right_back(){
         Assert.assertEquals("right back", PlayAnalyzer.onField(5));
     }
 
     @Test
+    @Ignore("Remove to run test")
     public void test_center_back(){
         Assert.assertEquals("center back", PlayAnalyzer.onField(3));
         Assert.assertEquals("center back", PlayAnalyzer.onField(4));
     }
 
     @Test
+    @Ignore("Remove to run test")
     public void test_midfielder(){
         Assert.assertEquals("midfielder", PlayAnalyzer.onField(6));
         Assert.assertEquals("midfielder", PlayAnalyzer.onField(7));
@@ -42,17 +36,32 @@ public class PlayAnalyzerTest {
     }
 
     @Test
+    @Ignore("Remove to run test")
     public void test_left_wing(){
         Assert.assertEquals("left wing", PlayAnalyzer.onField(9));
     }
 
     @Test
+    @Ignore("Remove to run test")
     public void test_striker(){
         Assert.assertEquals("striker", PlayAnalyzer.onField(10));
     }
 
     @Test
+    @Ignore("Remove to run test")
     public void test_right_wing(){
         Assert.assertEquals("right wing", PlayAnalyzer.onField(11));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Ignore("Remove to run test")
+    public void test_exception(){
+        PlayAnalyzer.onField(13);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Ignore("Remove to run test")
+    public void test_exception_negative_number(){
+        PlayAnalyzer.onField(-1);
     }
 }
