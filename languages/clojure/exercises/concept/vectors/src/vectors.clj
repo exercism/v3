9c17c5b824d7027796acca8b9ff137ba993ca566
Bobby Towers <porkostomus@mail.com>
2020-10-03 20:31:11 -0700

    [Clojure] add vectors exercise (#1835)
    * add vectors exercise
    
    * run prettier
    
    * format instructions.md
    
    * remove blank line
    
    * correct function name
    
    * remove blank line
    
    * run prettier for realsies
    
    * add trailing newline
    
    * format with prettier
    
    * run prettier from command line

---
+++ languages/clojure/exercises/concept/vectors/src/vectors.clj
@@ -0,0 +1,22 @@
+(ns vectors)
+
+(def last-week [0 2 5 3 7 8 4])
+(def birds-per-day [2 5 0 7 4 1])
+
+(defn today [birds]
+  (last birds))
+
+(defn inc-bird [birds]
+  (update birds (dec (count birds)) inc))
+
+(defn day-without-birds? [birds]
+  (pos? (count (filter zero? birds))))
+
+(defn n-days-count [birds n]
+  (reduce + (take n birds)))
+
+(defn busy-days [birds]
+  (count (filter #(>= % 5) birds)))
+
+(defn odd-week? [birds]
+  (= birds [1 0 1 0 1 0 1]))
