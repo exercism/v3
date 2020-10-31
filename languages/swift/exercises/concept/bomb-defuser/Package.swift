5dbfccbdedac78787b62d28cf69cacc44e6bf918
William D. Neumann <wneumann@gmail.com>
2020-10-31 14:11:32 -0500

    [Swift] Rename closures to bomb-defuser (#2581)
    * Renamed closures to bomb-defuser to reflect concept -> exercise switch
    
    * fix dead links and added new editor key to config
    
    * add new config
    
    * fixed broken link in anonymous_functions.md

--- languages/swift/exercises/concept/bomb-defuser/Package.swift
+++ languages/swift/exercises/concept/bomb-defuser/Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "closures",
+    name: "BombDefuser",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "closures",
-            targets: ["closures"]),
+            name: "BombDefuser",
+            targets: ["BombDefuser"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,12 +17,12 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "closures",
+            name: "BombDefuser",
             dependencies: []),
         .testTarget(
-            name: "closuresTests",
-            dependencies: ["closures"]),
+            name: "BombDefuserTests",
+            dependencies: ["BombDefuser"]),
     ]
 )
