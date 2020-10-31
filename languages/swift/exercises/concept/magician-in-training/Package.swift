fc81bf70d0910c5c236e01f5214d053367416e18
William D. Neumann <wneumann@gmail.com>
2020-10-31 11:07:41 -0500

    [Swift] Rename arrays to magician-in-training (#2573)
    * Renamed arrays to magician-in-training to reflect concept -> exercise switch
    
    * fix dead links and added new editor key to config
    
    * Update config.json
    
    Fix config, add accidentally deleted brace

--- languages/swift/exercises/concept/magician-in-training/Package.swift
+++ languages/swift/exercises/concept/magician-in-training/Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Arrays",
+    name: "MagicianInTraining",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Arrays",
-            targets: ["Arrays"]),
+            name: "MagicianInTraining",
+            targets: ["MagicianInTraining"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,12 +17,12 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Arrays",
+            name: "MagicianInTraining",
             dependencies: []),
         .testTarget(
-            name: "ArraysTests",
-            dependencies: ["Arrays"]),
+            name: "MagicianInTrainingTests",
+            dependencies: ["MagicianInTraining"]),
     ]
 )
