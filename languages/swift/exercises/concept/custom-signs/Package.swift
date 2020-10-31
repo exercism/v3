c979161012bbe01b64bb6fddb341fbc7339578be
William D. Neumann <wneumann@gmail.com>
2020-10-31 10:38:30 -0500

    [Swift] Rename strings-and-characters to custom-signs (#2571)
    * Renamed strings-and-characters to custom-signs to reflect concept -> exercise switch
    
    * fix dead links and added new editor key to config
    
    * corrected values for editor key

--- languages/swift/exercises/concept/custom-signs/Package.swift
+++ languages/swift/exercises/concept/custom-signs/Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "stringsAndCharacters",
+    name: "CustomSigns",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "stringsAndCharacters",
-            targets: ["stringsAndCharacters"]),
+            name: "CustomSigns",
+            targets: ["CustomSigns"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -17,12 +17,12 @@ let package = Package(
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "stringsAndCharacters",
+            name: "CustomSigns",
             dependencies: []),
         .testTarget(
-            name: "stringsAndCharactersTests",
-            dependencies: ["stringsAndCharacters"]),
+            name: "CustomSignsTests",
+            dependencies: ["CustomSigns"]),
     ]
 )
---
+++ languages/swift/exercises/concept/custom-signs/Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import CustomSignsTests
+import XCTest
+
+var tests = [XCTestCaseEntry]()
+tests += CustomSignsTests.allTests()
+XCTMain(tests)
