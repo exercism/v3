4c02c91f4778081b949357903f5cecb50e350476
Oleksandr Halushko <alexlesang@gmail.com>
2020-11-01 12:39:09 +0100

    [C++] Add numbers core exercise
    Apply review comments:
      - add stubs according to https://github.com/exercism/v3/blob/master/docs/concept-exercises.md#stub-implementation-file

--- languages/cpp/exercises/concept/numbers/numbers.cpp
+++ languages/cpp/exercises/concept/numbers/numbers.cpp
@@ -1,3 +1,15 @@
 #include "numbers.h"
 
-namespace assembly_line {}
+#include <exception>
+
+namespace assembly_line {
+double production_rate_per_hour(std::int32_t speed) {
+    throw std::logic_error{
+        "Not Implemented. Please implement production_rate_per_hour function"};
+}
+
+std::int32_t working_items_per_minute(std::int32_t speed) {
+    throw std::logic_error{
+        "Not Implemented. Please implement working_items_per_minute"};
+}
+}  // namespace assembly_line
--- languages/cpp/exercises/concept/numbers/numbers.h
+++ languages/cpp/exercises/concept/numbers/numbers.h
@@ -1,6 +1,11 @@
 #ifndef NUMBERS_H
 #define NUMBERS_H
 
-namespace assembly_line {}
+#include <cstdint>
+
+namespace assembly_line {
+double production_rate_per_hour(std::int32_t speed);
+std::int32_t working_items_per_minute(std::int32_t speed);
+}  // namespace assembly_line
 
 #endif /* NUMBERS_H */
