204a3aa763f5aa2c841780849e3e0c92f0988041
Tim Austin <tim@neenjaw.com>
2020-11-02 02:16:06 -0600

    rough draft of exercise

--- languages/javascript/exercises/concept/callbacks/fruit-picker.js
+++ languages/javascript/exercises/concept/callbacks/fruit-picker.js
@@ -1,5 +1,37 @@
-import { pick } from './grocer'
+import { checkStatus, checkInventory } from './grocer';
 
-export function handleResult(err, response) {}
+// Write an inline callback
+export function isServiceOpen() {
+  return checkStatus((openStatus) => {
+    if (openStatus === 'OPEN') {
+      return true;
+    }
 
-export function pickFruit(type, quantity, callback) {}
+    return false;
+  });
+}
+
+// Use a passed in callback
+export function pickFruit(variety, quantity, callback) {
+  return checkInventory(
+    {
+      type: 'fruit',
+      variety,
+      quantity,
+    },
+    callback
+  );
+}
+
+// Write a callback that can be reused
+export function handleInventoryResult(err, isAvailable) {
+  if (err) {
+    throw new Error(err);
+  }
+
+  if (isAvailable) {
+    return 'BUY';
+  } else {
+    return 'NO_OP';
+  }
+}
--- languages/javascript/exercises/concept/callbacks/grocer.js
+++ languages/javascript/exercises/concept/callbacks/grocer.js
@@ -1,1 +1,34 @@
-export function fruitQuery(query, callback) {}
+let STATUS = 'CLOSED';
+
+export function checkStatus() {
+  return STATUS;
+}
+
+export function setStatus(status) {
+  STATUS = status;
+}
+
+export function resetStatus() {
+  STATUS = 'CLOSED';
+}
+
+let LAST_QUERY = undefined;
+let RESPONSE = undefined;
+
+export function setResponse(response) {
+  RESPONSE = response;
+}
+
+export function getLastQuery() {
+  return LAST_QUERY;
+}
+
+export function resetQuery() {
+  LAST_QUERY = undefined;
+  RESPONSE = undefined;
+}
+
+export function checkInventory(query, callback) {
+  LAST_QUERY = query;
+  return callback(RESPONSE);
+}
