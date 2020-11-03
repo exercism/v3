97ec9d34c65985824e912caf763b364ae8908607
ceddlyburge <ceddlyburge@gmail.com>
2020-11-03 17:44:27 +0000

    Add explanatory comments around code options

--- languages/elm/exercises/concept/basics/src/Greet.elm
+++ languages/elm/exercises/concept/basics/src/Greet.elm
@@ -1,13 +1,11 @@
 -- This line 'exports' the `greet` function, so that 
 -- other modules (such as the tests) can use it
 module Greet exposing (greet)
--- or
--- module Basics exposing (..)
+-- It is possible to export everything with `module Greet exposing (..)`
 
--- you will need an `import` statement here to import the `toUpper` from the `String` module
+-- you will need an `import` statement here to import the `toUpper` function from the `String` module
 -- import String exposing (toUpper)
--- or
--- import String exposing (..)
+-- It is possible to import everything from the String module with `import String exposing (..)`
 
 greet : String -> String
 greet personToGreet =
