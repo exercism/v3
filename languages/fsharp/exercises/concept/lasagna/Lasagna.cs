09af33c754360ce76f045c45ae797c2b622e28cf
Erik Schierboom <erik_schierboom@hotmail.com>
2020-11-04 08:53:33 +0100

    [F#] Use correct extension

--- languages/fsharp/exercises/concept/lasagna/Lasagna.cs
+++
@@ -1,9 +0,0 @@
-module Lasagna
-
-// TODO: define the 'expectedMinutesInOven' binding
-
-// TODO: define the 'remainingMinutesInOven' function
-
-// TODO: define the 'preparationTimeInMinutes' function
-
-// TODO: define the 'elapsedTimeInMinutes' function
