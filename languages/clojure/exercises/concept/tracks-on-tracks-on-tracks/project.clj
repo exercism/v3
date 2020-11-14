0b26ccc7c29e25551608cfc90c48175db64a7cc1
porkostomus <porkostomus@mail.com>
2020-11-14 12:01:51 -0800

    use deps.edn consistently

--- languages/clojure/exercises/concept/tracks-on-tracks-on-tracks/project.clj
+++
@@ -1,4 +0,0 @@
-(defproject tracks-on-tracks-on-tracks "0.1.0-SNAPSHOT"
-  :description "concept exercise - lists"
-  ;; :url "https://github.com/exercism/clojure/tree/master/exercises/lists"
-  :dependencies [[org.clojure/clojure "1.10.0"]])
