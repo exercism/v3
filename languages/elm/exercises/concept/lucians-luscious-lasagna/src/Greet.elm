2c449f08f2b1274d4844e5d8ad0b8eda7762a588
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2020-11-29 17:34:13 +0100

    elm/basics: update code template

--- languages/elm/exercises/concept/lucians-luscious-lasagna/src/Greet.elm
+++
@@ -1,9 +0,0 @@
--- This line 'exports' the `greet` function, so that 
--- other modules (such as the tests) can use it
-module Greet exposing (greet)
--- It is possible to export everything with `module Greet exposing (..)`
-
--- you will need an `import` statement here to import the `toUpper` function from the `String` module
--- import String
-
--- TODO: define the 'greet' function
