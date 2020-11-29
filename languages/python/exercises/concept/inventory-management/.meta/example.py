e71d04ae123a890b00baf471210a358eabe02dd1
Valentin Pouget <3833193+valentin-p@users.noreply.github.com>
2020-11-29 18:51:54 +0100

    revert to two lines. Surround top-level function and class definitions with two blank lines.

--- languages/python/exercises/concept/inventory-management/.meta/example.py
+++ languages/python/exercises/concept/inventory-management/.meta/example.py
@@ -3,17 +3,20 @@ def create_inventory(items):
     add_items(inventory, items)
     return inventory
 
+
 def add_items(inventory, items):
     for item in items:
         inventory.setdefault(item, 0)
         inventory[item] += 1
     return inventory
 
+
 def delete_items(inventory, items):
     for item in items:
         if item in inventory:
             inventory[item] = max(inventory[item] - 1, 0)
     return inventory
+
 
 def list_inventory(inventory):
     output = list()
