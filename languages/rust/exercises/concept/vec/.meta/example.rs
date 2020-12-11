bd14b0b8060a8c05b304df0662c856d5d42cd15b
Eli Flanagan <efx@users.noreply.github.com>
2020-12-11 17:00:25 -0500

    try adding solution to example.rs

--- languages/rust/exercises/concept/vec/.meta/example.rs
+++ languages/rust/exercises/concept/vec/.meta/example.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+/// return a list of 5 languages to learn
+pub fn languages() -> Vec<String> {
+    ["Elixir", "JavaScript", "Lua", "Rust", "Scheme"]
+        .iter()
+        .map(|a_str| a_str.to_string())
+        .collect()
+}
+/// Gather your top languages to learn
+pub fn languages_top(count: usize) -> Result<Vec<String>, String> {
+    if count > languages().len() {
+        Err("Please specify a number less than or equal to 5.".to_string())
+    } else {
+        Ok(languages()[..count].to_vec())
     }
 }
