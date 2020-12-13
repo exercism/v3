72770eda39e9841f59f0d91f4019c7e55540143a
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-12-13 10:28:15 +0100

    Fix mix project name

--- languages/elixir/exercises/concept/chessboard/mix.exs
+++ languages/elixir/exercises/concept/chessboard/mix.exs
@@ -3,7 +3,7 @@ defmodule Chessboard.MixProject do
 
   def project do
     [
-      app: :captains_log,
+      app: :chessboard,
       version: "0.1.0",
       # elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
