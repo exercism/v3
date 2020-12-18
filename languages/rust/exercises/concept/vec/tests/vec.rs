94b7e9f2265378e67b273f79950653a71b5a6aa1
Eli Flanagan <efx@users.noreply.github.com>
2020-12-18 16:56:36 -0500

    expand example
    Address other learning points in https://github.com/exercism/v3/pull/2918#issuecomment-743479293

--- languages/rust/exercises/concept/vec/tests/vec.rs
+++ languages/rust/exercises/concept/vec/tests/vec.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use vec::{languages, languages_top};
+    use vec::{grab, humans_and_computers, languages, languages_ranked};
     #[test]
     #[ignore]
     fn it_has_languages() {
@@ -8,17 +8,29 @@ mod tests {
     }
     #[test]
     #[ignore]
-    fn has_top_count() {
-        assert_eq!(languages_top(3).is_ok(), true);
+    fn has_ranked() {
+        assert_eq!(&languages_ranked()[0], "1: Rust");
+        assert_eq!(&languages_ranked()[1], "2: JavaScript");
+        assert_eq!(&languages_ranked()[2], "3: Elixir");
+        assert_eq!(&languages_ranked()[3], "4: Lua");
+        assert_eq!(&languages_ranked()[4], "5: Scheme");
     }
     #[test]
     #[ignore]
-    fn has_error() {
-        assert_eq!(languages_top(9).is_err(), true);
+    fn gets_fist() {
+        assert_eq!(grab(0), "Rust");
     }
     #[test]
     #[ignore]
-    fn has_top_count_invalid() {
-        assert_eq!(languages_top(3).unwrap().len(), 3);
+    fn gets_last() {
+        assert_eq!(grab(4), "Scheme");
+    }
+    #[test]
+    #[ignore]
+    fn adds_human_languages() {
+        let both = humans_and_computers(["官话".to_string(), "كِسوَهِل".to_string()].to_vec());
+        assert_eq!(both[0], "Rust");
+        assert_eq!(both[5], "官话");
+        assert_eq!(both[6], "كِسوَهِل");
     }
 }
