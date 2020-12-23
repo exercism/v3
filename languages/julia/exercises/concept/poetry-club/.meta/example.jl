9d181298f07f27ea08fee48ea6526523cf6cde94
Sascha Mann <git@mail.saschamann.eu>
2020-12-23 11:41:02 +0100

    Use filter in example

--- languages/julia/exercises/concept/poetry-club/.meta/example.jl
+++ languages/julia/exercises/concept/poetry-club/.meta/example.jl
@@ -1,12 +1,7 @@
 front_response(line) = line[begin]
 front_password(poem) = titlecase(join(front_response.(split(poem, "\n"))))
 
-function remove_punctuation(s)
-    for punct in ".,!"
-        s = replace(s, punct => "")
-    end
-    s
-end
+remove_punctuation(s) = filter(!ispunct, s)
 
 back_response(line) = remove_punctuation(line)[end]
 back_password(poem) = titlecase(join(back_response.(split(poem, "\n")))) * ", please."
