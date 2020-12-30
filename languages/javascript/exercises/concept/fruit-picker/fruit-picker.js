51858b371ee002e439d141da7c9274dc47fd1010
Tim Austin <tim@neenjaw.com>
2020-12-30 11:45:39 -0600

    update fruit-picker.js

--- languages/javascript/exercises/concept/fruit-picker/fruit-picker.js
+++ languages/javascript/exercises/concept/fruit-picker/fruit-picker.js
@@ -2,8 +2,6 @@
 
 import { checkStatus, checkInventory } from './grocer';
 
-// ADD YOUR CODE BELOW HERE.
-
 /**
  * Returns the service status as a boolean value
  * @return {boolean}
@@ -13,11 +11,12 @@ export function isServiceOnline() {
 }
 
 /**
- * pick a fruit using the checkInventory API
+ * Pick a fruit using the checkInventory API
+ *
  * @param {string} variety
- * @param {string} quantity
- * @param {function} callback
- * @return {unknown} the result from checkInventory
+ * @param {number} quantity
+ * @param {InventoryCallback} callback
+ * @return {AvailabilityAction} the result from checkInventory
  */
 export function pickFruit(variety, quantity, callback) {
   throw new Error('Implement the pickFruit function');
@@ -26,19 +25,20 @@ export function pickFruit(variety, quantity, callback) {
 /**
  * This is a callback function to be passed to the checkInventory API
  * handles the next step once the inventory is known
- * @param {string} err
- * @param {Object} isAvailable
- * @return {string} whether the fruit was purchased 'PURCHASE' or 'NOOP'
+ * @param {string | null} err
+ * @param {boolean} isAvailable
+ * @return {AvailabilityAction} whether the fruit was purchased 'PURCHASE' or 'NOOP'
  */
 export function purchaseInventoryIfAvailable(err, isAvailable) {
   throw new Error('Implement the purchaseInventoryIfAvailable function');
 }
 
 /**
- * pick a fruit, and if it is available, purchase it
+ * Pick a fruit, and if it is available, purchase it
+ *
  * @param {string} variety
  * @param {number} quantity
- * @return {string} whether the fruit was purchased 'PURCHASE' or 'NOOP'
+ * @return {AvailabilityAction} whether the fruit was purchased 'PURCHASE' or 'NOOP'
  */
 export function pickAndPurchaseFruit(variety, quantity) {
   throw new Error('Implement the pickAndPurchaseFruit function');
