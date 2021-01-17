a03855d000fd631627b46790f44672b151c736b1
shubhsk88 <shubham88singh@gmail.com>
2021-01-18 02:31:26 +0530

    Completing the tests for the problem

--- languages/javascript/exercises/concept/equality/pokemon-mania.spec.js
+++ languages/javascript/exercises/concept/equality/pokemon-mania.spec.js
@@ -56,4 +56,50 @@ describe('equality', () => {
       expect(findSecretNumber(42, expected)).toBe(true);
     });
   });
+  describe('Second Input', () => {
+    const pokemon1 = {
+      name: 'Vulpix',
+      drought: '23',
+
+      xp: 90,
+      height: 6,
+      weight: 100,
+      hustle: null,
+      lightingRod: null,
+      swarm: null,
+      specialAttack: 'Ember',
+      flashFire: 40,
+    };
+    const pokemon2 = {
+      name: 'Charmander',
+      xp: 63,
+      height: 6,
+      weight: 100,
+      hustle: null,
+      lightingRod: null,
+      swarm: null,
+      torrent: null,
+      blaze: '20',
+      growl: '54',
+      specialAttack: 'Solar Power',
+    };
+    const expectedValue = {
+      height: 6,
+      weight: 100,
+      hustle: null,
+      lightingRod: null,
+      swarm: null,
+    };
+
+    test(`checkPokemonData(${pokemon1},${pokemon2})`, () => {
+      expect(checkPokemonData(pokemon1, pokemon2)).toEqual(expectedValue);
+    });
+    const expected = { height: 6, weight: 100 };
+    test(`validCommonDetails(${expectedValue})`, () => {
+      expect(validCommonDetails(expectedValue)).toEqual(expected);
+    });
+    test(`validCommonDetails(${expected})`, () => {
+      expect(findSecretNumber(23, expected)).toBe(false);
+    });
+  });
 });
