ca2e116eaa65e6379146cbcfa6026ee0540a63bc
shubhsk88 <shubham88singh@gmail.com>
2021-01-18 02:21:12 +0530

    Adding the first test case

---
+++ languages/javascript/exercises/concept/equality/pokemon-mania.spec.js
@@ -0,0 +1,59 @@
+import {
+  checkPokemonData,
+  validCommonDetails,
+  findSecretNumber,
+} from './pokemon-mania';
+
+describe('equality', () => {
+  describe('First Input', () => {
+    const pokemon1 = {
+      name: 'Pikachu',
+      staticShock: '30',
+      xp: 246,
+      height: 4,
+      weight: 120,
+      hustle: null,
+      lightingRod: '42',
+      swarm: null,
+      specialAttack: 'Tackle',
+    };
+    const pokemon2 = {
+      name: 'Raichu',
+      staticShock: '30',
+      xp: 246,
+      height: 8,
+      weight: 120,
+      hustle: null,
+      lightingRod: '42',
+      swarm: null,
+      rage: '45',
+      specialAttack: 'Tackle',
+    };
+    const expectedValue = {
+      static: 30,
+      xp: 246,
+      weight: 120,
+      hustle: null,
+      lightingRod: 42,
+      swarm: null,
+      specialAttack: 'Tackle',
+    };
+
+    test(`checkPokemonData(${pokemon1},${pokemon2})`, () => {
+      expect(checkPokemonData(pokemon1, pokemon2)).toEqual(expectedValue);
+    });
+    const expected = {
+      static: '30',
+      xp: 246,
+      weight: 120,
+      lightingRod: '42',
+      specialAttack: 'Tackle',
+    };
+    test(`validCommonDetails(${expectedValue})`, () => {
+      expect(validCommonDetails(expectedValue)).toEqual(expected);
+    });
+    test(`validCommonDetails(${expected})`, () => {
+      expect(findSecretNumber(42, expected)).toBe(true);
+    });
+  });
+});
