515fb30ccfc2bd121c5af2e8b53375296c543507
porkostomus <porkostomus@mail.com>
2021-01-20 18:47:53 -0800

    swap top two cards

--- languages/clojure/exercises/concept/elyses-destructured-enchantments/src/exercism/elyses_destructured_enchantments.clj
+++ languages/clojure/exercises/concept/elyses-destructured-enchantments/src/exercism/elyses_destructured_enchantments.clj
@@ -6,7 +6,28 @@
   (let [[first] deck]
     first))
 
+(defn second-card
+  "Returns the second card from deck."
+  [deck]
+  (let [[_ second] deck]
+    second))
+
+(defn swap-top-two-cards
+  "Returns the deck with first two items reversed."
+  [deck]
+  (let [[a b & rest] deck]
+    (vec (conj rest a b))))
+
+(let [[a b & rest] [10 4 3 7 8]]
+  (vec (conj rest a b)))
+
 (comment
   (first-card [3])
   (first-card [8 3 9 5])
+  (second-card [10 4])
+  (second-card [2 5 1 6])
+  (second-card [])
+  (second-card [8])
+  (swap-top-two-cards [3 6])
+  (swap-top-two-cards [10 4 3 7 8])
   )
--- languages/clojure/exercises/concept/elyses-destructured-enchantments/test/exercism/elyses_destructured_enchantments_test.clj
+++ languages/clojure/exercises/concept/elyses-destructured-enchantments/test/exercism/elyses_destructured_enchantments_test.clj
@@ -5,3 +5,13 @@
 (deftest first-card-test
   (is (= (first-card [3]) 3))
   (is (= (first-card [8 3 9 5]) 8)))
+
+(deftest second-card-test
+  (is (= (second-card [10 4]) 4))
+  (is (= (second-card [2 5 1 6]) 5))
+  (is (nil? (second-card [])))
+  (is (nil? (second-card [8]))))
+
+(deftest swap-top-two-cards-test
+  (is (= (swap-top-two-cards [3 6]) [6 3]))
+  (is (= (swap-top-two-cards [10 4 3 7 8]) [4 10 3 7 8])))
