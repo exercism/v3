49f3187ae60046ce9a7c0ca7c0cb9f833b1a0250
Sascha Mann <git@mail.saschamann.eu>
2021-01-23 12:09:33 +0100

    annalyn1: Use Bool matrices

--- languages/julia/exercises/concept/annalyns-infiltration/runtests.jl
+++ languages/julia/exercises/concept/annalyns-infiltration/runtests.jl
@@ -8,55 +8,55 @@ include("game.jl")
 end
 
 @testset "spying" begin
-    character_state_combinations = [
-        (false, false, false, false),
-        (false, false, true, true),
-        (false, true, false, true),
-        (false, true, true, true),
-        (true, false, false, true),
-        (true, false, true, true),
-        (true, true, true, true),
+    character_state_combinations = Bool[
+        0 0 0 0;
+        0 0 1 1;
+        0 1 0 1;
+        0 1 1 1;
+        1 0 0 1;
+        1 0 1 1;
+        1 1 1 1;
     ]
 
-    for state in character_state_combinations
+    for state in eachrow(character_state_combinations)
         @test can_spy(state[1:3]...) == state[4]
     end
 end
 
 @testset "signaling prisoner" begin
-    character_state_combinations = [
-        (false, false, false),
-        (false, true, true),
-        (true, false, false),
-        (true, true, false),
+    character_state_combinations = Bool[
+        0 0 0;
+        0 1 1;
+        1 0 0;
+        1 1 0;
     ]
 
-    for state in character_state_combinations
+    for state in eachrow(character_state_combinations)
         @test can_signal_prisoner(state[1:2]...) == state[3]
     end
 end
 
 @testset "freeing prisoner" begin
-    character_state_combinations = [
-        (false, false, false, false, false),
-        (false, false, false, true, true),
-        (false, false, true, false, true),
-        (false, false, true, true, true),
-        (false, true, false, false, false),
-        (false, true, false, true, false),
-        (false, true, true, false, false),
-        (false, true, true, true, false),
-        (true, false, false, false, false),
-        (true, false, false, true, true),
-        (true, false, true, false, false),
-        (true, false, true, true, true),
-        (true, true, false, false, false),
-        (true, true, false, true, false),
-        (true, true, true, false, false),
-        (true, true, true, true, false),
+    character_state_combinations = Bool[
+        0 0 0 0 0;
+        0 0 0 1 1;
+        0 0 1 0 1;
+        0 0 1 1 1;
+        0 1 0 0 0;
+        0 1 0 1 0;
+        0 1 1 0 0;
+        0 1 1 1 0;
+        1 0 0 0 0;
+        1 0 0 1 1;
+        1 0 1 0 0;
+        1 0 1 1 1;
+        1 1 0 0 0;
+        1 1 0 1 0;
+        1 1 1 0 0;
+        1 1 1 1 0;
     ]
 
-    for state in character_state_combinations
+    for state in eachrow(character_state_combinations)
         @test can_free_prisoner(state[1:4]...) == state[5]
     end
 end
