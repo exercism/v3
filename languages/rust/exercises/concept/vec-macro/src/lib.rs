e6bce12f9790f68704133338a06ce610bb77b3e2
Eli Flanagan <efx@users.noreply.github.com>
2021-01-26 14:46:09 -0500

    mirror lib.rs changes

--- languages/rust/exercises/concept/vec-macro/src/lib.rs
+++ languages/rust/exercises/concept/vec-macro/src/lib.rs
@@ -7,7 +7,7 @@ fn create_empty() -> Vec<u8> {
 
 /// Create a buffer of `count` zeroes.
 ///
-/// Applications use these for filling and modifying data
+/// Applications often use buffers when serializing data to send over the network.
 fn create_buffer(count: usize) -> Vec<u8> {
     unimplemented!()
 }
@@ -15,6 +15,7 @@ fn create_buffer(count: usize) -> Vec<u8> {
 /// Create a vector containing the first five digits of the Fibonacci sequence.
 ///
 /// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
+/// Its first five elements are `1, 1, 2, 3, 5`.
 fn fibonacci() -> Vec<u8> {
     unimplemented!()
 }
