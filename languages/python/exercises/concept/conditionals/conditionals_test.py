ffe1713ce099fad13961dee3cbd76b94c7563a6a
sachsom95 <sachin.soman@ucdconnect.ie>
2021-01-28 01:48:22 +0000

    Generated the scaffolding for test cases

--- languages/python/exercises/concept/conditionals/conditionals_test.py
+++ languages/python/exercises/concept/conditionals/conditionals_test.py
@@ -1,1 +1,23 @@
 # unit test here
+import unittest
+from conditionals import *
+
+
+class TestConditionals(unittest.TestCase):
+    def test_is_criticality_balanced_set1(self):
+        pass
+
+    def test_is_criticality_balanced_set2(self):
+        pass
+
+    def test_reactor_efficency_set1(self):
+        pass
+
+    def test_reactor_efficency_set2(self):
+        pass
+
+    def test_fail_safe_set1(self):
+        pass
+
+    def test_fail_safe_set1(self):
+        pass
