3079700cd120349da7cb63bd90e219cab7815c59
sachsom95 <sachin.soman@ucdconnect.ie>
2021-01-28 00:40:34 +0000

    added the functions definitions and the parameters

--- languages/python/exercises/concept/conditionals/conditionals.py
+++ languages/python/exercises/concept/conditionals/conditionals.py
@@ -1,1 +1,10 @@
-# main code here
+def is_criticality_balanced(temprature, neutrons_emitted_per_second):
+    pass
+
+
+def reactor_efficency(voltage, current, theoretical_max_power):
+    pass
+
+
+def fail_safe(temperature, neutrons_produced_per_second, threshold):
+    pass
